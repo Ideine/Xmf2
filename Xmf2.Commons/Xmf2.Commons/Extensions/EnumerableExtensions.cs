1f4e19392ea7d45150a4d2c864620cf65c4087d4
Paul Leman <paul.leman@ideine.fr>
2015-11-10 17:12:16 +0100

    Add Xmf2.Commons

---
+++ Xmf2.Commons/Xmf2.Commons/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xmf2.Commons.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> lst)
+        {
+            if (lst == null)
+                return new T[0];
+            else
+                return lst;
+        }
+
+    }
+}
---
+++ Xmf2.Commons/Xmf2.Commons/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xmf2.Commons.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsEmptyOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+        public static bool In(this string str, params string[] lst)
+        {
+            return lst.Contains(str);
+        }
+    }
+}
