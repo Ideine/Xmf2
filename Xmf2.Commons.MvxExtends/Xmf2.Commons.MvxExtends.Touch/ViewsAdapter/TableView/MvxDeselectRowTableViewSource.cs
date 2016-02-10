f551ca7257f4a2483eef2a1ae3c37eeaa5e7a9fb
M'COLO MARI Mouhamadi <mcolomari.mouhamadi@live.fr>
2016-02-10 10:12:14 +0100

    Added in constructor a default height for each row "tableRowHeight"

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewsAdapter/TableView/MvxDeselectRowTableViewSource.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewsAdapter/TableView/MvxDeselectRowTableViewSource.cs
@@ -7,12 +7,16 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewsAdapter.TableView
 	public class MvxDeselectRowTableViewSource :  Cirrious.MvvmCross.Binding.Touch.Views.MvxSimpleTableViewSource
 	{
 
-		public MvxDeselectRowTableViewSource(UITableView tableView, string nibName, string cellIdentifier = null, NSBundle bundle = null) : base (tableView, nibName, cellIdentifier, bundle)
+		private readonly int _tableRowHeight;
+
+		public MvxDeselectRowTableViewSource(UITableView tableView, string nibName, string cellIdentifier = null, NSBundle bundle = null,int tableRowHeight = 50) : base (tableView, nibName, cellIdentifier, bundle)
 		{
+			_tableRowHeight = tableRowHeight;
 		}
 
-		public MvxDeselectRowTableViewSource(UITableView tableView, Type cellType, string cellIdentifier = null): base(tableView, cellType, cellIdentifier)
+		public MvxDeselectRowTableViewSource(UITableView tableView, Type cellType, string cellIdentifier = null,int tableRowHeight = 50): base(tableView, cellType, cellIdentifier)
 		{
+			_tableRowHeight = tableRowHeight;
 		}
 
 		public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
@@ -23,7 +27,7 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewsAdapter.TableView
 
 		public override nfloat GetHeightForRow (UITableView tableView, NSIndexPath indexPath)
 		{
-			return 50;
+			return _tableRowHeight;
 		}
 
 		public override UIView GetViewForFooter (UITableView tableView, nint section)
