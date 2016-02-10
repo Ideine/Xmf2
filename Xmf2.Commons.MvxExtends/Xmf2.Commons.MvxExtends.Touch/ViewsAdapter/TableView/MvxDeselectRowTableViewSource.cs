6c936ffcad8b869f99b4c08fc7a81e4153c727b4
M'COLO MARI Mouhamadi <mcolomari.mouhamadi@live.fr>
2016-02-10 09:41:32 +0100

    Added MvxDeselectRowTableViewSource : a class which deselect a selected row

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewsAdapter/TableView/MvxDeselectRowTableViewSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UIKit;
+using Foundation;
+
+namespace Xmf2.Commons.MvxExtends.Touch.ViewsAdapter.TableView
+{
+	public class MvxDeselectRowTableViewSource :  Cirrious.MvvmCross.Binding.Touch.Views.MvxSimpleTableViewSource
+	{
+
+		public MvxDeselectRowTableViewSource(UITableView tableView, string nibName, string cellIdentifier = null, NSBundle bundle = null) : base (tableView, nibName, cellIdentifier, bundle)
+		{
+		}
+
+		public MvxDeselectRowTableViewSource(UITableView tableView, Type cellType, string cellIdentifier = null): base(tableView, cellType, cellIdentifier)
+		{
+		}
+
+		public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
+		{
+			tableView.DeselectRow(indexPath, true);
+			base.RowSelected(tableView, indexPath);
+		}
+
+		public override nfloat GetHeightForRow (UITableView tableView, NSIndexPath indexPath)
+		{
+			return 50;
+		}
+
+		public override UIView GetViewForFooter (UITableView tableView, nint section)
+		{
+			return new UIView();
+		}
+	}
+}
+
