7047da9ff0642d7c4af7ac0b40ba1e9ef0657d93
M'COLO MARI Mouhamadi <mcolomari.mouhamadi@live.fr>
2016-03-16 14:08:32 +0100

    Code Design MvxDeselectRowTableViewSource

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewsAdapter/TableView/MvxDeselectRowTableViewSource.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewsAdapter/TableView/MvxDeselectRowTableViewSource.cs
@@ -7,32 +7,19 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewsAdapter.TableView
 	public class MvxDeselectRowTableViewSource :  Cirrious.MvvmCross.Binding.Touch.Views.MvxSimpleTableViewSource
 	{
 
-		private readonly int _tableRowHeight;
 
-		public MvxDeselectRowTableViewSource(UITableView tableView, string nibName, string cellIdentifier = null, NSBundle bundle = null,int tableRowHeight = 50) : base (tableView, nibName, cellIdentifier, bundle)
+		public MvxDeselectRowTableViewSource (UITableView tableView, string nibName, string cellIdentifier = null, NSBundle bundle = null) : base (tableView, nibName, cellIdentifier, bundle)
 		{
-			_tableRowHeight = tableRowHeight;
 		}
 
-		public MvxDeselectRowTableViewSource(UITableView tableView, Type cellType, string cellIdentifier = null,int tableRowHeight = 50): base(tableView, cellType, cellIdentifier)
+		public MvxDeselectRowTableViewSource (UITableView tableView, Type cellType, string cellIdentifier = null) : base (tableView, cellType, cellIdentifier)
 		{
-			_tableRowHeight = tableRowHeight;
 		}
 
 		public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
 		{
-			tableView.DeselectRow(indexPath, true);
-			base.RowSelected(tableView, indexPath);
-		}
-
-		public override nfloat GetHeightForRow (UITableView tableView, NSIndexPath indexPath)
-		{
-			return _tableRowHeight;
-		}
-
-		public override UIView GetViewForFooter (UITableView tableView, nint section)
-		{
-			return new UIView();
+			tableView.DeselectRow (indexPath, true);
+			base.RowSelected (tableView, indexPath);
 		}
 	}
 }
