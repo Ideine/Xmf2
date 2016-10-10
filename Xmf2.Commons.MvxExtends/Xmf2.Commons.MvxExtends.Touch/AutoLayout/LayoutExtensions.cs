baf513fc0e725523015f24844d30874688fd07ef
Julien Mialon <julien.mialon@ideine.fr>
2016-10-10 18:57:35 +0200

    add fluent layout for touch

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/LayoutExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UIKit;
+
+public static class CustomAutoLayoutExtensions
+{
+	public static void CenterAndFillWidth(this UIView containerView, params UIView[] views)
+	{
+		if (views == null)
+		{
+			throw new ArgumentNullException(nameof(views));
+		}
+
+		foreach (UIView view in views)
+		{
+			containerView.ConstrainLayout(() =>
+										  view.CenterX() == containerView.CenterX()
+										  && view.Width() == containerView.Width()
+										 );
+		}
+	}
+
+	public static void VerticalFlow(this UIView containerView, params UIView[] views)
+	{
+		if (views == null)
+		{
+			throw new ArgumentNullException(nameof(views));
+		}
+
+		if (views.Length == 0)
+		{
+			throw new ArgumentException("views must contains at least one element", nameof(views));
+		}
+
+		containerView.AnchorTop(views[0]);
+		containerView.AnchorBottom(views[views.Length - 1]);
+
+		for (int i = 1; i < views.Length; ++i)
+		{
+			containerView.VerticalSpace(views[i - 1], views[i]);
+		}
+	}
+
+	public static void AnchorTop(this UIView containerView, UIView view, int margin = 0)
+	{
+		containerView.ConstrainLayout(() => view.Top() == containerView.Top() + margin);
+	}
+
+	public static void AnchorBottom(this UIView containerView, UIView view, int margin = 0)
+	{
+		containerView.ConstrainLayout(() => containerView.Bottom() == view.Bottom() + margin);
+	}
+
+	public static void VerticalSpace(this UIView containerView, UIView top, UIView bottom, int margin = 0)
+	{
+		containerView.ConstrainLayout(() => bottom.Top() == top.Bottom() + margin);
+	}
+}
