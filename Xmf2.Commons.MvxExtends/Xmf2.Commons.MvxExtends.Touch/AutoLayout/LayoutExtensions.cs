d5248acdf8e57d87675878118d3eb70c210d2e2e
Julien Mialon <julien.mialon@ideine.fr>
2016-10-21 17:10:26 +0200

    extend autolayout extensions

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/LayoutExtensions.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/LayoutExtensions.cs
@@ -40,34 +40,122 @@ public static class CustomAutoLayoutExtensions
 		}
 	}
 
-	public static void AnchorTop(this UIView containerView, UIView view, int margin = 0)
+	public static UIView ViewsEqualWidth(this UIView containerView, params UIView[] views)
+	{
+		if (views == null)
+		{
+			throw new ArgumentNullException(nameof(views));
+		}
+
+		if (views.Length < 2)
+		{
+			throw new ArgumentException("views must contains at least two elements", nameof(views));
+		}
+
+		for (int i = 1; i < views.Length; ++i)
+		{
+			UIView v1 = views[i - 1];
+			UIView v2 = views[i];
+
+			containerView.ConstrainLayout(() => v1.Width() == v2.Width());
+		}
+
+		return containerView;
+	}
+
+	public static UIView AnchorTop(this UIView containerView, UIView view, int margin = 0)
 	{
 		containerView.ConstrainLayout(() => view.Top() == containerView.Top() + margin);
+		return containerView;
 	}
 
-	public static void AnchorBottom(this UIView containerView, UIView view, int margin = 0)
+	public static UIView AnchorBottom(this UIView containerView, UIView view, int margin = 0)
 	{
 		containerView.ConstrainLayout(() => containerView.Bottom() == view.Bottom() + margin);
+		return containerView;
 	}
 
-	public static void AnchorRight(this UIView containerView, UIView view, int margin = 0)
+	public static UIView AnchorRight(this UIView containerView, UIView view, int margin = 0)
 	{
 		containerView.ConstrainLayout(() => containerView.Right() == view.Right() + margin);
+		return containerView;
 	}
 
-	public static void AnchorLeft(this UIView containerView, UIView view, int margin = 0)
+	public static UIView AnchorLeft(this UIView containerView, UIView view, int margin = 0)
 	{
 		containerView.ConstrainLayout(() => view.Left() == containerView.Left() + margin);
+		return containerView;
 	}
 
-	public static void VerticalSpace(this UIView containerView, UIView top, UIView bottom, int margin = 0)
+	public static UIView CenterHorizontally(this UIView containerView, UIView view)
+	{
+		containerView.ConstrainLayout(() => view.CenterX() == containerView.CenterX());
+		return containerView;
+	}
+
+	public static UIView CenterVertically(this UIView containerView, UIView view)
+	{
+		containerView.ConstrainLayout(() => view.CenterY() == containerView.CenterY());
+		return containerView;
+	}
+
+	public static UIView FillWidth(this UIView containerView, UIView view, int margin = 0)
+	{
+		containerView.ConstrainLayout(() => containerView.Width() == view.Width() + margin);
+		return containerView;
+	}
+
+	public static UIView FillHeight(this UIView containerView, UIView view, int margin = 0)
+	{
+		containerView.ConstrainLayout(() => containerView.Height() == view.Height() + margin);
+		return containerView;
+	}
+
+	public static UIView VerticalSpace(this UIView containerView, UIView top, UIView bottom, int margin = 0)
 	{
 		containerView.ConstrainLayout(() => bottom.Top() == top.Bottom() + margin);
+		return containerView;
+	}
+
+	public static UIView HorizontalSpace(this UIView containerView, UIView left, UIView right, int margin = 0)
+	{
+		containerView.ConstrainLayout(() => right.Left() == left.Right() + margin);
+		return containerView;
 	}
 
 	public static UIView ConstrainHeight(this UIView view, int height)
 	{
 		view.ConstrainLayout(() => view.Height() == height);
+		return view;
+	}
+
+	public static UIView ConstrainWidth(this UIView view, int width)
+	{
+		view.ConstrainLayout(() => view.Width() == width);
+		return view;
+	}
+
+	public static UIView ConstrainMinHeight(this UIView view, int height)
+	{
+		view.ConstrainLayout(() => view.Height() >= height);
+		return view;
+	}
+
+	public static UIView ConstrainMinWidth(this UIView view, int width)
+	{
+		view.ConstrainLayout(() => view.Width() >= width);
+		return view;
+	}
+
+	public static UIView ConstrainMaxHeight(this UIView view, int height)
+	{
+		view.ConstrainLayout(() => view.Height() <= height);
+		return view;
+	}
+
+	public static UIView ConstrainMaxWidth(this UIView view, int width)
+	{
+		view.ConstrainLayout(() => view.Width() <= width);
 		return view;
 	}
 }
