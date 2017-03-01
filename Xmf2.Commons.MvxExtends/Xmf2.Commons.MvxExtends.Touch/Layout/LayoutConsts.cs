1f0151f6a038c216ccd4fdb6888d2369c4c2eeb9
ma.coutanceau <ma.coutanceau@ideine.fr>
2017-03-01 16:55:15 +0100

    Ajout d'une classe référençant les tailles d'éléments graphiques garantis par Apple (une seule entrée pour l'instant)

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/Layout/LayoutConsts.cs
@@ -0,0 +1,14 @@
+namespace Xmf2.Commons.MvxExtends.Touch.Layout
+{
+	/// <summary>
+	/// Ensemble de constantes garanties par Apple.
+	/// </summary>
+	public class LayoutConsts
+	{
+		//See : http://ivomynttinen.com/blog/ios-design-guidelines#tab-bar
+		/// <summary>
+		/// <see cref="UIKit.UITabBar/>.Frame.Height
+		/// </summary>
+		public const int UITabBar_DefaultHeight = 49;
+	}
+}
