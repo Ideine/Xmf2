ee5a6c725502b17a7815b3f64bc2ae3b35c90a04
ma.coutanceau <ma.coutanceau@ideine.fr>
2017-03-06 17:17:12 +0100

    Ajout d'un NavigationJsonConverter permettant une sérialisation/déserialisation de base pour les IMvxViewModelInitializer<TInit>

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Helpers/NavigationJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using MvvmCross.Platform.Platform;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Xmf2.Commons.MvxExtends.Helpers
+{
+	public class NavigationJsonConverter : IMvxJsonConverter
+	{
+		private static readonly JsonSerializerSettings Settings;
+
+		static NavigationJsonConverter()
+		{
+			Settings = new JsonSerializerSettings
+			{
+				ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+				DateFormatHandling = DateFormatHandling.IsoDateFormat
+			};
+		}
+
+		public T DeserializeObject<T>(string inputText)
+		{
+			return JsonConvert.DeserializeObject<T>(inputText, Settings);
+		}
+
+		public string SerializeObject(object toSerialise)
+		{
+			return JsonConvert.SerializeObject(toSerialise, Formatting.None, Settings);
+		}
+
+		public object DeserializeObject(Type type, string inputText)
+		{
+			return JsonConvert.DeserializeObject(inputText, type, Settings);
+		}
+
+		public T DeserializeObject<T>(Stream stream)
+		{
+			var serializer = JsonSerializer.Create(Settings);
+
+			using (var sr = new StreamReader(stream))
+			using (var jsonTextReader = new JsonTextReader(sr))
+			{
+				return serializer.Deserialize<T>(jsonTextReader);
+			}
+		}
+	}
+}
