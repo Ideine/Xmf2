9a3666da125e4feda11076a733e5db5e26eea201
ma.coutanceau <ma.coutanceau@ideine.fr>
2017-03-17 14:32:41 +0100

    Adding a constant to NotConverter

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/NotConverter.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/NotConverter.cs
@@ -5,6 +5,8 @@ namespace Xmf2.Commons.MvxExtends.Converters
 {
 	public class NotConverter : MvxValueConverter<bool, bool>
 	{
+		public const string ConverterName = nameof(NotConverter);
+
 		protected override bool Convert(bool value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
 		{
 			return !value;
