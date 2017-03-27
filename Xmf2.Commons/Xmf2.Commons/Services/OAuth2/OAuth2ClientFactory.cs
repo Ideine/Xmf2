c5627bb88d3f0420159b7a992efa1d79fcaeff5e
Julien Mialon <julien.mialon@ideine.fr>
2017-03-27 12:21:25 +0200

    Add OAuth2 client factory without a base url

--- Xmf2.Commons/Xmf2.Commons/Services/OAuth2/OAuth2ClientFactory.cs
+++ Xmf2.Commons/Xmf2.Commons/Services/OAuth2/OAuth2ClientFactory.cs
@@ -17,6 +17,16 @@ namespace Xmf2.Commons.Services.OAuth2
 			};
 		}
 
+		public static IOAuth2Client CreateClient(OAuth2ConfigurationBase configuration, Action<Method, string, string> logMethod = null)
+		{
+			return new OAuth2RestClient()
+			{
+				Configuration = configuration,
+				LogRequest = logMethod,
+				Timeout = TimeSpan.FromSeconds(30)
+			};
+		}
+
 		public static Action<Method, string, string> CreateDefaultLog(ILogger logger)
 		{
 			return (method, uri, body) => logger.LogInfo(message: $"HTTP {method} {uri} with content {body}");
