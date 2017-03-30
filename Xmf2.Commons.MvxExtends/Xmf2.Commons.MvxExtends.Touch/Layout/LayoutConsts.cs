a9dc62973b1379368293f7dc1bc6fd7d246a06e2
mcoutanceau <ma.coutanceau@ideine.fr>
2017-03-30 16:40:58 +0200

    Adding a layout const for status bar

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/Layout/LayoutConsts.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/Layout/LayoutConsts.cs
@@ -10,9 +10,12 @@ namespace Xmf2.Commons.MvxExtends.Touch.Layout
 		/// <see cref="UIKit.UITabBar/>.Frame.Height
 		/// </summary>
 		public const int UITabBar_DefaultHeight = 49;
+
 		/// <summary>
 		/// Epaisseur utilisée pour un séparateur visuel entre champs.
 		/// </summary>
 		public const int UISeparator_Height = 1;
+
+		public const int UIStatusBar_DefaultHeight = 20;
 	}
 }
