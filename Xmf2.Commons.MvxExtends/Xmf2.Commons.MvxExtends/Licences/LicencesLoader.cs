9e01ba1d7c5bc87f6ff256ce81985ca8ba224b53
M'COLO MARI Mouhamadi <mcolomari.mouhamadi@live.fr>
2017-04-04 17:51:20 +0200

    Added LicenceReaderService

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Licences/LicencesLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xmf2.Commons.MvxExtends.Models;
+
+namespace Xmf2.Commons.MvxExtends.Licences
+{
+    public static class LicencesLoader
+    {
+        public static String GetLicensesText(Notices notices, bool showFullLicenseText = true)
+        {
+            return NoticesHtmlBuilder
+                    .Create()
+                    .SetShowFullLicenseText(showFullLicenseText)
+                    .SetStyle(LicenceStyle.LicenceDelfaultStyle)
+                    .SetNotices(notices)
+                    .SetClickableUrl(false)
+                    .Build();
+        }
+    }
+}
