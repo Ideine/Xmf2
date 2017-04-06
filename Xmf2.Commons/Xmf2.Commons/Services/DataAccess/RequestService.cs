44d30e6e10350e53ab9faf0d8669fb290e63646b
Julien Mialon <julien.mialon@ideine.fr>
2017-04-06 10:47:10 +0200

    fix request without authentication

--- Xmf2.Commons/Xmf2.Commons/Services/DataAccess/RequestService.cs
+++ Xmf2.Commons/Xmf2.Commons/Services/DataAccess/RequestService.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using RestSharp.Portable;
 using Xmf2.Commons.ErrorManagers;
+using Xmf2.Rest.OAuth2;
 
 namespace Xmf2.Commons.Services
 {
@@ -25,11 +26,21 @@ namespace Xmf2.Commons.Services
 
 		public virtual Task<IRestResponse> Execute(IRestRequest request, CancellationToken ct, bool withAuthentication = true)
 		{
+			if (!withAuthentication)
+			{
+				request.AddHeader(OAuth2Authenticator.NO_AUTH_HEADER, true);
+			}
+
 			return ErrorManager.ExecuteAsync(() => _client.Execute(request, ct));
 		}
 
 		public virtual Task<IRestResponse<T>> Execute<T>(IRestRequest request, CancellationToken ct, bool withAuthentication = true)
 		{
+			if (!withAuthentication)
+			{
+				request.AddHeader(OAuth2Authenticator.NO_AUTH_HEADER, true);
+			}
+
 			return ErrorManager.ExecuteAsync(() => _client.Execute<T>(request, ct));
 		}
 	}
