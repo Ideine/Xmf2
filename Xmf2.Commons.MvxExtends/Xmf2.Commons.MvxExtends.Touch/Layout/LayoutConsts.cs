125170a8455b38250f3a1cc5fd3d3e11ed2f506b
mcoutanceau <ma.coutanceau@ideine.fr>
2017-04-13 13:44:10 +0200

    Adding new class UITextViewWithPlaceHolder

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/Layout/LayoutConsts.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/Layout/LayoutConsts.cs
@@ -16,6 +16,9 @@ namespace Xmf2.Commons.MvxExtends.Touch.Layout
 		/// </summary>
 		public const int UISeparator_Height = 1;
 
+		/// <summary>
+		/// Epaisseur de la StatusBar (en haut de l'écran) selon le standard d'Apple.
+		/// </summary>
 		public const int UIStatusBar_DefaultHeight = 20;
 	}
 }
---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewComponents/UITextViewWithPlaceHolder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
+{
+	public class UITextViewWithPlaceHolder : UITextView
+	{
+		private readonly UILabel _placeholderLabel;
+
+		public UITextViewWithPlaceHolder() : base()
+		{
+			this._placeholderLabel = new UILabel() { BackgroundColor = UIColor.Clear };
+			this.PlaceholderColor = UIColor.Gray;
+			this.Started += this.OnStarted;
+			this.Ended += this.OnEnded;
+			this.Add(this._placeholderLabel);
+		}
+
+		public string Placeholder { get { return _placeholderLabel.Text; } set { _placeholderLabel.Text = value; } }
+		public UIColor PlaceholderColor { get { return _placeholderLabel.TextColor; } set { _placeholderLabel.TextColor = value; } }
+		public UIFont PlaceholderFont { get { return _placeholderLabel.Font; } set { _placeholderLabel.Font = value; } }
+
+		public override void Draw(CGRect rect)
+		{
+			base.Draw(rect);
+			var inset	   = this.TextContainerInset;
+			var leftInset  = this.TextContainer.LineFragmentPadding + inset.Left;
+			var rightInset = this.TextContainer.LineFragmentPadding + inset.Right;
+			var placeHolderMaxSize = new CGSize(width: this.Frame.Width  - (leftInset + rightInset)
+											 , height: this.Frame.Height - (inset.Top + inset.Bottom));
+			var size = _placeholderLabel.Text.StringSize(_placeholderLabel.Font, placeHolderMaxSize, UILineBreakMode.WordWrap);
+			this._placeholderLabel.Frame = new CGRect(new CGPoint(leftInset, inset.Top), size);
+		}
+
+		private void OnStarted(object sender, EventArgs e)
+		{
+			this._placeholderLabel.Hidden = true;
+		}
+
+		private void OnEnded(object sender, EventArgs e)
+		{
+			this._placeholderLabel.Hidden = !string.IsNullOrWhiteSpace(this.Text);
+		}
+	}
+}
