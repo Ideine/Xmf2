4c5e1d51f8e715115a19ed1e0a09e08c45e55aa8
mcoutanceau <ma.coutanceau@ideine.fr>
2017-04-14 11:41:51 +0200

    Adding StringHelper.Join

---
+++ Xmf2.Commons/Xmf2.Commons/Helpers/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace System
+{
+	public static class StringHelper
+	{
+		public static string Join(string separator, bool removeEmptyEntries, params string[] values)
+		{
+			return String.Join(separator, values.Where(s => !String.IsNullOrEmpty(s)));
+		}
+		public static string Join(string separator, bool removeEmptyEntries, IEnumerable<string> values)
+		{
+			return String.Join(separator, values.Where(s => !String.IsNullOrEmpty(s)));
+		}
+	}
+}
