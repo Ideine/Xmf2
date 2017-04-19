f472cea5465ed08c8ed9e87b375606abd4cfb360
Julien Mialon <julien.mialon@ideine.fr>
2017-04-19 13:53:30 +0200

    add extension method for insets

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/CreatorExtensions.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/CreatorExtensions.cs
@@ -552,6 +552,11 @@ public static class CreatorExtensions
         return view;
     }
 
-    #endregion
+	#endregion
+
+	public static UIEdgeInsets Add(this UIEdgeInsets insets, float top = 0f, float left = 0f, float bottom = 0f, float right = 0f)
+	{
+		return new UIEdgeInsets(top + insets.Top, left + insets.Left, bottom + insets.Bottom, right + insets.Right);
+	}
 
 }
