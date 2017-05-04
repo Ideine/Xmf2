bf386a52d9068495f142e3a9193a166f6c59e51c
mcoutanceau <ma.coutanceau@ideine.fr>
2017-05-04 17:28:06 +0200

    Added methods on workbook. Updated NavBarWithTwoActions to flip right image on top button and added a default highlitgh color for text

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewComponents/NavBarWithTwoActions.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewComponents/NavBarWithTwoActions.cs
@@ -3,11 +3,14 @@ using System.Windows.Input;
 using Xmf2.Commons.Extensions;
 using UIKit;
 using Xmf2.Commons.MvxExtends.Touch.Layout;
+using CoreGraphics;
 
 namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 {
 	public class NavBarWithTwoActions : UIView
 	{
+
+		private static readonly UIColor _DEFAULT_TEXT_HIGHLIGHT_COLOR = UIColorExtension.ColorFromHex(0x757575);
 		private bool _layoutDone;
 
 		private readonly UIView _container;
@@ -15,88 +18,75 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 		#region Public Properties
 
 		public Action LeftAction { get; set; }
-
 		public Action RightAction { get; set; }
-
 		public ICommand LeftCommand
 		{
 			get { return null; }
 			set { LeftAction = () => value?.TryExecute(); }
 		}
-
 		public ICommand RightCommand
 		{
 			get { return null; }
 			set { RightAction = () => value?.TryExecute(); }
 		}
-
 		public string LeftActionTitle
 		{
 			get { return LeftButton?.Title(UIControlState.Normal); }
 			set { LeftButton.WithTitle(value); }
 		}
-
 		public string RightActionTitle
 		{
 			get { return RightButton?.Title(UIControlState.Normal); }
 			set { RightButton.WithTitle(value); }
 		}
-
 		public string TextTitle
 		{
 			get { return Title?.Text; }
 			set { Title.WithText(value); }
 		}
-
 		public UIFont TextTitleFont
 		{
 			get { return Title?.Font; }
 			set { Title.WithFont(value); }
 		}
-
 		public bool HasLeftAction
 		{
 			get { return LeftButton?.Hidden ?? false; }
 			set { LeftButton.Hidden = !value; }
 		}
-
 		public bool HasRightAction
 		{
 			get { return RightButton?.Hidden ?? false; }
 			set { RightButton.Hidden = !value; }
 		}
-
 		public UIButton LeftButton { get; }
-
 		public UIButton RightButton { get; }
-
 		public UILabel Title { get; }
 
 		#endregion Public Properties
 
-		public NavBarWithTwoActions(string withLeftImage)
+		private static readonly CGAffineTransform _flipTransform = CGAffineTransform.MakeScale(-1.0f, 1.0f);
+		public NavBarWithTwoActions(string leftImage, string rightImage)
 		{
-			bool hasBackImage = !string.IsNullOrEmpty(withLeftImage);
-
-			this.LeftButton  = this.CreateButton().WithTitle(LeftActionTitle).OnClick(OnClickLeftButton);
-			this.RightButton = this.CreateButton().WithTitle(RightActionTitle).OnClick(OnClickRightButton);
+			this.LeftButton  = this.CreateButton().WithTitle(LeftActionTitle).OnClick(OnClickLeftButton).WithTextColorHighlight(_DEFAULT_TEXT_HIGHLIGHT_COLOR);
+			this.RightButton = this.CreateButton().WithTitle(RightActionTitle).OnClick(OnClickRightButton).WithTextColorHighlight(_DEFAULT_TEXT_HIGHLIGHT_COLOR);
 			this.Title = this.CreateLabel().WithText(TextTitle).WithAlignment(UITextAlignment.Center);
 
-			if (hasBackImage)
+			bool hasLeftImage = !string.IsNullOrEmpty(leftImage);
+			bool hasRightImage = !string.IsNullOrEmpty(rightImage);
+			if (hasLeftImage)
 			{
-				this.LeftButton.WithImage(withLeftImage);
-				//LeftButton.ContentEdgeInsets = LeftButton.ContentEdgeInsets.Add(right: 4);
-				//LeftButton.TitleEdgeInsets = new UIEdgeInsets(0, 4, 0, -4);
+				LeftButton.WithImage(leftImage);
 			}
-			else
+			if (hasRightImage)
 			{
-				this.LeftButton.ContentEdgeInsets = new UIEdgeInsets(0, 8, 0, 8);
-				this.RightButton.ContentEdgeInsets = new UIEdgeInsets(0, 8, 0, 8);
+				RightButton.WithImage(rightImage);
+				RightButton.Transform = _flipTransform;
+				RightButton.TitleLabel.Transform = _flipTransform;
+				RightButton.ImageView.Transform = _flipTransform;
 			}
-
 			_container = this.CreateView()
 							 .WithSubviews(Title, LeftButton, RightButton);
-
 			this.Add(_container);
 		}
 
@@ -118,13 +108,14 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 			_container.CenterVertically(LeftButton)
 					  .CenterVertically(Title)
 					  .CenterVertically(RightButton)
-					  .FillHeight(LeftButton)
-					  .FillHeight(RightButton);
+					  //.FillHeight(LeftButton)
+					  //.FillHeight(RightButton)
+					  ;
 
 			//Horizontal Layout
 			this.CenterAndFillWidth(_container);
-			_container.AnchorLeft(LeftButton)
-					  .AnchorRight(RightButton)
+			_container.AnchorLeft(LeftButton, 30 / 2)
+					  .AnchorRight(RightButton, 30 / 2)
 					  .CenterAndFillWidth(Title);
 		}
 
@@ -168,7 +159,7 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 
 		public static NavBarWithTwoActions Create(string title)
 		{
-			return new NavBarWithTwoActions(null)
+			return new NavBarWithTwoActions(null, null)
 			{
 				TextTitle = title,
 				HasLeftAction = false,
@@ -178,7 +169,7 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 
 		public static NavBarWithTwoActions CreateSimpleBack(string title, string leftText)
 		{
-			return new NavBarWithTwoActions(null)
+			return new NavBarWithTwoActions(null, null)
 			{
 				TextTitle = title,
 				LeftActionTitle = leftText,
@@ -186,9 +177,9 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 			};
 		}
 
-		public static NavBarWithTwoActions Create(string title, string leftText, string withBackImage = null)
+		public static NavBarWithTwoActions Create(string title, string leftText, string leftImage = null)
 		{
-			return new NavBarWithTwoActions(withBackImage)
+			return new NavBarWithTwoActions(leftImage, null)
 			{
 				TextTitle = title,
 				LeftActionTitle = leftText,
@@ -196,9 +187,9 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 			};
 		}
 
-		public static NavBarWithTwoActions Create(string title, string leftText, string rightText, string withBackImage = null)
+		public static NavBarWithTwoActions Create(string title, string leftText, string rightText, string leftImage = null)
 		{
-			return new NavBarWithTwoActions(withBackImage)
+			return new NavBarWithTwoActions(leftImage, null)
 			{
 				TextTitle = title,
 				LeftActionTitle = leftText,
@@ -206,9 +197,9 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 			};
 		}
 
-		public static NavBarWithTwoActions Create(string title, string leftText, Action leftAction, string withBackImage = null)
+		public static NavBarWithTwoActions Create(string title, string leftText, Action leftAction, string leftImage = null)
 		{
-			return new NavBarWithTwoActions(withBackImage)
+			return new NavBarWithTwoActions(leftImage, null)
 			{
 				TextTitle = title,
 				LeftActionTitle = leftText,
@@ -217,9 +208,9 @@ namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
 			};
 		}
 
-		public static NavBarWithTwoActions Create(string title, string leftText, string rightText, Action leftAction, Action rightAction, string withBackImage = null)
+		public static NavBarWithTwoActions Create(string title, string leftText, string rightText, Action leftAction, Action rightAction, string leftImage, string rightImage)
 		{
-			return new NavBarWithTwoActions(withBackImage)
+			return new NavBarWithTwoActions(leftImage, rightImage)
 			{
 				TextTitle = title,
 				LeftActionTitle = leftText,
