79b13dcd3993843195d42485e3b8bfef4178fbdb
Valentin Jubert <valentin.jubert@ideine.fr>
2017-05-04 11:50:02 +0200

    Improvement of islistemptyconverter

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/IsListEmptyConverter.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/IsListEmptyConverter.cs
@@ -4,11 +4,29 @@ using System.Collections;
 
 namespace Xmf2.Commons.MvxExtends.Converters
 {
-	public class IsListEmptyConverter : MvxValueConverter<ICollection, bool>
-	{
-		protected override bool Convert(ICollection value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return value.Count == 0;
-		}
-	}
+    public class IsListEmptyConverter : MvxValueConverter<ICollection, bool>
+    {
+
+        public const string ConverterName = "IsListEmpty";
+
+        public const string ReverseParameter = "Not";
+
+        protected override bool Convert(ICollection value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool returnValue;
+            if (value == null)
+            {
+                returnValue = true;
+            }
+            else
+            {
+                returnValue = value.Count == 0;
+            }
+            if (ReverseParameter.Equals(parameter as string))
+            {
+                returnValue = !returnValue;
+            }
+            return returnValue;
+        }
+    }
 }
