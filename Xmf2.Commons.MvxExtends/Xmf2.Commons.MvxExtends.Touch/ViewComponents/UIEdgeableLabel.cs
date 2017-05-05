fe6e9dd3ccf9f86dfc2cc3c3102892ae40e25c4b
mcoutanceau <ma.coutanceau@ideine.fr>
2017-05-05 14:48:48 +0200

    [iOS] Added UIEdgeableLabel + ObjectExtensions.CastTo<T>(...)

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ViewComponents/UIEdgeableLabel.cs
@@ -0,0 +1,46 @@
+﻿using CoreGraphics;
+using Foundation;
+using System;
+using UIKit;
+
+namespace Xmf2.Commons.MvxExtends.Touch.ViewComponents
+{
+	public class UIEdgeableLabel : UILabel
+	{
+		public UIEdgeableLabel() : base() { }
+		public UIEdgeableLabel(NSCoder coder) : base(coder) { }
+		public UIEdgeableLabel(CGRect frame) : base(frame) { }
+		protected UIEdgeableLabel(NSObjectFlag t) : base(t) { }
+
+		private UIEdgeInsets _edgeInsets = UIEdgeInsets.Zero;
+		public UIEdgeInsets EdgeInsets
+		{
+			get { return _edgeInsets; }
+			set
+			{
+				_edgeInsets = value;
+				this.InvalidateIntrinsicContentSize();
+			}
+		}
+
+		public override CGRect TextRectForBounds(CGRect bounds, nint numberOfLines)
+		{
+			var rect = base.TextRectForBounds(EdgeInsets.InsetRect(bounds), numberOfLines);
+			return new CGRect(x: rect.X - EdgeInsets.Left,
+							  y: rect.Y - EdgeInsets.Top,
+							  width: rect.Width + EdgeInsets.Left + EdgeInsets.Right,
+							  height: rect.Height + EdgeInsets.Top + EdgeInsets.Bottom);
+		}
+
+		public override void DrawText(CGRect rect)
+		{
+			base.DrawText(this.EdgeInsets.InsetRect(rect));
+		}
+
+		public UIEdgeableLabel WithEdgeInsets(nfloat top, nfloat left, nfloat bottom, nfloat right)
+		{
+			this.EdgeInsets = new UIEdgeInsets(top, left, bottom, right);
+			return this;
+		}
+	}
+}
---
+++ Xmf2.Commons/Xmf2.Commons/Extensions/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class ObjectExtensions
+{
+	public static T CastTo<T>(this Object value)
+	{
+		return (T)value;
+	}
+}
