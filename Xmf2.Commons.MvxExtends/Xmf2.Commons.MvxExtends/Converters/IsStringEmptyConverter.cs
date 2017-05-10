6162366f05721ce2da5b0d0ea40f9c32d28f6bb2
Valentin Jubert <valentin.jubert@ideine.fr>
2017-05-10 17:10:17 +0200

    Update IsStringEmpty

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/IsStringEmptyConverter.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/IsStringEmptyConverter.cs
@@ -1,13 +1,20 @@
-﻿using MvvmCross.Platform.Converters;
+﻿﻿using MvvmCross.Platform.Converters;
 using System;
 
 namespace Xmf2.Commons.MvxExtends.Converters
 {
 	public class IsStringEmptyConverter : MvxValueConverter<string, bool>
 	{
-		protected override bool Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return string.IsNullOrEmpty(value);
-		}
+        protected override bool Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ("Trim".Equals(parameter as string))
+            {
+                return string.IsNullOrWhiteSpace(value);
+            }
+            else
+            {
+                return string.IsNullOrEmpty(value);
+            }
+        }
 	}
 }
