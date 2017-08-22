d53e93a8dbf8c17eec5973c346b6031033969be0
Julien Mialon <julien.mialon@ideine.fr>
2017-08-22 12:04:23 +0200

    Update iOS notification service to handle deeplink on remote and local notification

--- Xmf2.Commons/Xmf2.Commons.iOS/Services/NotificationService.cs
+++ Xmf2.Commons/Xmf2.Commons.iOS/Services/NotificationService.cs
@@ -1,12 +1,8 @@
-﻿/*
-using System;
+﻿using System;
 using Foundation;
-using MvvmCross.iOS.Platform;
-using MvvmCross.Platform;
-using MvvmCross.Platform.Platform;
 using UIKit;
 using UserNotifications;
-using Xmf2.Commons.MvxExtends.Services;
+using Xmf2.Commons.Services;
 
 namespace Xmf2.Commons.MvxExtends.Touch.Services
 {
@@ -27,7 +23,7 @@ namespace Xmf2.Commons.MvxExtends.Touch.Services
 		}
 	}
 
-	public class NotificationAppDelegate : MvxApplicationDelegate
+	public abstract class NotificationAppDelegate : UIApplicationDelegate
 	{
 		public override void DidRegisterUserNotificationSettings(UIApplication application, UIUserNotificationSettings notificationSettings)
 		{
@@ -37,55 +33,137 @@ namespace Xmf2.Commons.MvxExtends.Touch.Services
 			}
 			else
 			{
-				GetNotificationService()?.SetToken(null);
+				NotificationService?.SetToken(null);
 			}
+		}
+
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		{
+			if(UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+			{
+				UNUserNotificationCenter.Current.Delegate = new LocalNotificationDelegate(DeeplinkFromNotification);
+			}
+
+			if(launchOptions != null)
+			{
+				HandleLaunchOptions(launchOptions);
+			}
+
+			return true;
+		}
+
+		private void HandleLaunchOptions(NSDictionary launchOptions)
+		{
+			if (launchOptions == null)
+			{
+				return;
+			}
+
+			if (!UIDevice.CurrentDevice.CheckSystemVersion(10, 0)) // iOS < 10.0
+			{
+				if (launchOptions.ContainsKey(UIApplication.LaunchOptionsLocalNotificationKey)
+				   && launchOptions[UIApplication.LaunchOptionsLocalNotificationKey] is UILocalNotification notification)
+				{
+					if (notification.UserInfo != null)
+					{
+						DeeplinkFromNotification(notification.UserInfo);
+					}
+				}
+			}
+
+			if (launchOptions.ContainsKey(UIApplication.LaunchOptionsRemoteNotificationKey)
+			   && launchOptions[UIApplication.LaunchOptionsRemoteNotificationKey] is NSDictionary userInfo)
+			{
+				if (userInfo != null)
+				{
+					DeeplinkFromNotification(userInfo);
+				}
+			}
+		}
+
+		public override void ReceivedLocalNotification(UIApplication application, UILocalNotification notification)
+		{
+			base.ReceivedLocalNotification(application, notification);
+
+			if (notification.UserInfo != null)
+			{
+				DeeplinkFromNotification(notification.UserInfo);
+			}
+		}
+
+		protected virtual void DeeplinkFromNotification(NSDictionary userInfo)
+		{
+			
+		}
+
+		protected virtual void BackroundProcessFromNotification(NSDictionary userInfo)
+		{
+			
 		}
 
 		public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
 		{
-			GetNotificationService()?.SetToken(TokenToString(deviceToken));
+			NotificationService?.SetToken(TokenToString(deviceToken));
 		}
 
 		public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
 		{
-			MvxTrace.Trace($"Failed to register for remote notifications: {error}");
-			GetNotificationService()?.SetToken(null);
+			System.Diagnostics.Debug.WriteLine($"Failed to register for remote notifications: {error}");
+			NotificationService?.SetToken(null);
 		}
 
 		public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
 		{
 			if (application.ApplicationState != UIApplicationState.Active)
 			{
-				//TODO: add some deeplinking code
+				BackroundProcessFromNotification(userInfo);
 				return;
 			}
 
-			if (userInfo.ContainsKey(new NSString("aps")))
-			{
-				//Get the aps dictionary
-				NSDictionary aps = userInfo.ObjectForKey(new NSString("aps")) as NSDictionary;
+			NSString apsKey = new NSString("aps");
+			NSString alertKey = new NSString("alert");
+			NSString locArgsKey = new NSString("loc-args");
+			NSString locKeyKey = new NSString("loc-key");
 
+			if (userInfo.ContainsKey(apsKey) && userInfo[apsKey] is NSDictionary aps)
+			{
 				string alert = string.Empty;
 
-				if (aps.ContainsKey(new NSString("alert")))
+				if (aps.ContainsKey(alertKey))
 				{
-					alert = (aps[new NSString("alert")] as NSString).ToString();
+					if (aps[alertKey] is NSString alertText)
+					{
+						alert = alertText?.ToString();
+					}
+					else if(aps[alertKey] is NSDictionary alertDictionary)
+					{
+						NSString localizedKey = null;
+						if(alertDictionary.ContainsKey(locKeyKey) && alertDictionary[locKeyKey] is NSString rawLocKey)
+						{
+							localizedKey = rawLocKey;
+						}
+						if(localizedKey != null && alertDictionary.ContainsKey(locArgsKey) && alertDictionary[locArgsKey] is NSArray args)
+						{
+							string format = NSBundle.MainBundle.LocalizedString(localizedKey, null) ?? localizedKey;
+							alert = NSString.LocalizedFormat(format, args).ToString();
+						}
+					}
 				}
 
 				//Manually show an alert
 				if (!string.IsNullOrEmpty(alert))
 				{
-					ShowNotification(alert);
+					ShowNotification(alert, userInfo);
 				}
 			}
 		}
 
-		protected virtual void ShowNotification(string alert)
+		protected virtual void ShowNotification(string alert, NSDictionary userInfo)
 		{
-			ShowLocalNotification(alert);
+			ShowLocalNotification(alert, userInfo);
 		}
 
-		protected void ShowLocalNotification(string text)
+		protected void ShowLocalNotification(string text, NSDictionary userInfo)
 		{
 			if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
 			{
@@ -93,7 +171,8 @@ namespace Xmf2.Commons.MvxExtends.Touch.Services
 				var notification = new UNMutableNotificationContent
 				{
 					Body = text,
-					Sound = UNNotificationSound.Default
+					Sound = UNNotificationSound.Default,
+					UserInfo = userInfo
 				};
 
 				var trigger = UNTimeIntervalNotificationTrigger.CreateTrigger(1, false);
@@ -101,7 +180,7 @@ namespace Xmf2.Commons.MvxExtends.Touch.Services
 
 				notificationCenter.AddNotificationRequest(notificationRequest, (err) => 
 				{
-					MvxTrace.Trace($"LocalNotificationError: Code={err.Code} / Description={err.Description} / FailureReason={err.LocalizedFailureReason}");
+					System.Diagnostics.Debug.WriteLine($"LocalNotificationError: Code={err.Code} / Description={err.Description} / FailureReason={err.LocalizedFailureReason}");
 				});
 			}
 			else
@@ -109,24 +188,14 @@ namespace Xmf2.Commons.MvxExtends.Touch.Services
 				UILocalNotification notification = new UILocalNotification
 				{
 					AlertBody = text,
-					SoundName = UILocalNotification.DefaultSoundName
+					SoundName = UILocalNotification.DefaultSoundName,
+					UserInfo = userInfo
 				};
 				UIApplication.SharedApplication.PresentLocalNotificationNow(notification);
 			}
 		}
 
-		protected void ShowAlertNotification(string title, string text, string button)
-		{
-			UIAlertView notificationAlert = new UIAlertView(title, text, null, button, null);
-			notificationAlert.Show();
-		}
-
-		private INotificationService GetNotificationService()
-		{
-			INotificationService result;
-			Mvx.TryResolve(out result);
-			return result;
-		}
+		protected abstract INotificationService NotificationService { get; }
 
 		private string TokenToString(NSData deviceToken)
 		{
@@ -137,6 +206,48 @@ namespace Xmf2.Commons.MvxExtends.Touch.Services
 
 			return deviceTokenString;
 		}
+
+		public class LocalNotificationDelegate : UNUserNotificationCenterDelegate
+		{
+			private Action<NSDictionary> _notificationCallback;
+
+			public LocalNotificationDelegate(Action<NSDictionary> notificationCallback)
+			{
+				_notificationCallback = notificationCallback;
+			}
+
+			protected LocalNotificationDelegate(NSObjectFlag t) : base(t)
+			{
+			}
+
+			protected internal LocalNotificationDelegate(IntPtr handle) : base(handle)
+			{
+			}
+
+			public override void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
+			{
+				if (response.IsDismissAction)
+				{
+					return;
+				}
+
+				if (response.IsDefaultAction)
+				{
+					NSDictionary userInfo = response.Notification?.Request?.Content?.UserInfo;
+
+					if (userInfo == null)
+					{
+						return;
+					}
+
+					_notificationCallback?.Invoke(userInfo);
+				}
+			}
+
+			public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+			{
+				completionHandler(UNNotificationPresentationOptions.Alert | UNNotificationPresentationOptions.Sound | UNNotificationPresentationOptions.Badge);
+			}
+		}
 	}
 }
-*/
