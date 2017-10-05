7865d2a42352ff426073f52756f4e9d3beda2218
Valentin Jubert <valentin.jubert@ideine.fr>
2017-10-05 14:14:20 +0200

    Add base component to xmd rx droid

---
+++ src/Xmf2.Rx.Droid/Services/BaseViewPresenter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using Android.App;
+using Android.Content;
+using Android.Support.V7.App;
+using Splat;
+
+namespace Xmf2.Rx.Droid.Services
+{
+	public class BaseViewPresenter
+	{
+		private readonly Lazy<ICurrentActivity> _currentActivity;
+
+		protected Activity CurrentActivity => _currentActivity.Value.Activity;
+
+		public BaseViewPresenter()
+		{
+			_currentActivity = new Lazy<ICurrentActivity>(Locator.Current.GetService<ICurrentActivity>);
+		}
+
+		protected virtual void ShowView(Type viewType, bool clearHistory = false)
+		{
+			var intent = CreateIntentForViewType(viewType, clearHistory);
+			if (intent != null)
+			{
+				Show(intent);
+			}
+		}
+
+		protected virtual Intent CreateIntentForViewType(Type viewType, bool clearHistory = false)
+		{
+			var activity = CurrentActivity;
+			if (activity == null)
+			{
+				Debug.WriteLine("Cannot Resolve current activity");
+				return null;
+			}
+			var intent = new Intent(activity, viewType);
+			if (clearHistory)
+			{
+				intent.SetFlags(ActivityFlags.ClearTask | ActivityFlags.NewTask);
+			}
+			return intent;
+		}
+
+		protected virtual void Show(Intent intent)
+		{
+			var activity = CurrentActivity;
+			if (activity == null)
+			{
+				Debug.WriteLine("Cannot Resolve current activity");
+				return;
+			}
+			activity.StartActivity(intent);
+		}
+
+		protected virtual void ShowView(Android.Support.V4.App.DialogFragment view, string tag)
+		{
+			var appCompatActivity = CurrentActivity as AppCompatActivity;
+
+			if (appCompatActivity != null)
+			{
+				var fm = appCompatActivity.SupportFragmentManager;
+				if (fm.FindFragmentByTag(tag) == null)
+				{
+					view.Show(fm, tag);
+				}
+			}
+		}
+
+		public virtual void Close()
+		{
+			if (CurrentActivity == null)
+			{
+				Debug.WriteLine("Ignoring close for viewmodel - current page is not the view for the requested viewmodel");
+				return;
+			}
+			CurrentActivity.Finish();
+		}
+	}
+}
---
+++ src/Xmf2.Rx.Droid/Services/LifecycleMonitor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using Android.App;
+using Android.Content;
+
+namespace Xmf2.Rx.Droid.Services
+{
+	public interface ICurrentActivity
+	{
+		Activity Activity { get; }
+	}
+
+	public interface ILifecycleMonitor
+	{
+		void OnCreate(Activity activity);
+
+		void OnStart(Activity activity);
+
+		void OnRestart(Activity activity);
+
+		void OnResume(Activity activity);
+
+		void OnPause(Activity activity);
+
+		void OnStop(Activity activity);
+
+		void OnDestroy(Activity activity);
+	}
+
+	public class LifecycleMonitor : ILifecycleMonitor, ICurrentActivity
+	{
+		public Activity Activity { get; private set; }
+
+		public LifecycleMonitor(Context context)
+		{
+			Activity = context as Activity;
+		}
+
+		public void OnCreate(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnCreate {activity.GetType().Name}");
+			Activity = activity;
+		}
+
+		public virtual void OnStart(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnStart {activity.GetType().Name}");
+			Activity = activity;
+		}
+
+		public virtual void OnRestart(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnRestart {activity.GetType().Name}");
+			Activity = activity;
+		}
+
+		public virtual void OnResume(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnResume {activity.GetType().Name}");
+			Activity = activity;
+		}
+
+		public virtual void OnPause(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnPause {activity.GetType().Name}");
+		}
+
+		public virtual void OnStop(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnStop {activity.GetType().Name}");
+		}
+
+		public virtual void OnDestroy(Activity activity)
+		{
+			Debug.WriteLine($"[AcivityLifeCycle] OnDestroy {activity.GetType().Name}");
+
+			if (this.Activity == activity)
+			{
+				Activity = null;
+			}
+		}
+	}
+}
