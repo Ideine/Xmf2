24e948da85783a60048abb57f8bb4a3f2f8213cb
Julien Mialon <julien.mialon@ideine.fr>
2017-10-05 16:15:09 +0200

    Add assets service

---
+++ src/Xmf2.Rx.Droid/Services/Resource/AssetService.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Content.Res;
+using Xmf2.Rx.Services.Resources;
+
+namespace Xmf2.Rx.Droid.Services.Resource
+{
+	public class AssetService : IAssetService
+	{
+		private readonly AssetManager _assetManager;
+
+		public AssetService(Context context)
+		{
+			_assetManager = context.Assets;
+		}
+		
+		public async Task<Stream> ReadAsset(string path)
+		{
+			using (Stream inputStream = _assetManager.Open(path))
+			{
+				MemoryStream result = new MemoryStream();
+				await inputStream.CopyToAsync(result);
+
+				result.Seek(0, SeekOrigin.Begin);
+				return result;
+			}
+		}
+	}
+}
---
+++ src/Xmf2.Rx/Services/Resources/IAssetService.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Xmf2.Rx.Services.Resources
+{
+	public interface IAssetService
+	{
+		Task<Stream> ReadAsset(string path);
+	}
+}
