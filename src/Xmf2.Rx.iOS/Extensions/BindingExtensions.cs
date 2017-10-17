975ff55fdf63a22cef350b3691a2564b0cb3a584
Julien Mialon <julien.mialon@ideine.fr>
2017-10-17 10:33:48 +0200

    Add extensions for binding Rx

---
+++ src/Xmf2.Rx.iOS/Extensions/BindingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using ReactiveUI;
+
+// ReSharper disable once CheckNamespace => Extension class
+namespace UIKit
+{
+	public static class BindingExtensions
+	{
+		public static IDisposable OnClickCommand(this UIControl button, Func<ICommand> getCommand)
+		{
+			return button.Events().TouchUpInside
+				.OnTaskThread()
+				.Subscribe(_ => getCommand()?.TryExecute());
+		}
+	}
+}
