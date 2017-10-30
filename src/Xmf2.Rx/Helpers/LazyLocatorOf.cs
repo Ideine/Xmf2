09799753f15b8c20287ed585e4f9ec13dcb70db0
mcoutanceau <ma.coutanceau@ideine.fr>
2017-10-30 14:24:11 +0100

    [RX] Added LazyLocatorOf<T> to reduce common syntax writing of Lazy<T> (allow to skip writing of 'new Lazy<TService>(Locator.Current.GetService<TService>)' and is autocompleted by IDE)

---
+++ src/Xmf2.Rx/Helpers/LazyLocatorOf.cs
@@ -0,0 +1,13 @@
+﻿using Splat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xmf2.Rx.Helpers
+{
+    public class LazyLocatorOf<TService>
+    {
+		private readonly Lazy<TService> _service = new Lazy<TService>(Locator.Current.GetService<TService>);
+		public TService Value { get => _service.Value; }
+	}
+}
