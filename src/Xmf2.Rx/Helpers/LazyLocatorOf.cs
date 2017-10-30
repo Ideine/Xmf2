3c9c0b7503c3f1d9c1b6489cd130efc1215ef3dc
mcoutanceau <ma.coutanceau@ideine.fr>
2017-10-30 15:06:05 +0100

    Added LazyLocatorOf

---
+++ src/Xmf2.Rx/Helpers/LazyLocatorOf.cs
@@ -0,0 +1,11 @@
+﻿using Splat;
+using System;
+
+namespace Xmf2.Rx.Helpers
+{
+	public class LazyLocatorOf<TService>
+    {
+		private readonly Lazy<TService> _service = new Lazy<TService>(Locator.Current.GetService<TService>);
+		public TService Value { get => _service.Value; }
+    }
+}
