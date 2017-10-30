2d966899f3b096d40023cfb64b3d4a6f29149ab2
Valentin Jubert <valentin.jubert@ideine.fr>
2017-10-30 17:37:29 +0100

    Improve lazy locator of

--- src/Xmf2.Rx/Helpers/LazyLocatorOf.cs
+++ src/Xmf2.Rx/Helpers/LazyLocatorOf.cs
@@ -4,8 +4,8 @@ using System;
 namespace Xmf2.Rx.Helpers
 {
 	public class LazyLocatorOf<TService>
-    {
+	{
 		private readonly Lazy<TService> _service = new Lazy<TService>(Locator.Current.GetService<TService>);
-		public TService Value { get => _service.Value; }
-    }
+		public TService Value => _service.Value;
+	}
 }
