2cd35d1df55738ed00721579910eef2ab70ebd0b
Valentin Jubert <valentin.jubert@ideine.fr>
2017-10-31 10:21:36 +0100

    ReactiveFragment

---
+++ src/Xmf2.Rx.Droid/BaseView/BaseReactiveFragment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Views;
+using Xmf2.Rx.ViewModels;
+
+namespace Xmf2.Rx.Droid.BaseView
+{
+	public abstract class BaseReactiveFragment<TViewModel> : XMFFragment<TViewModel> where TViewModel : BaseViewModel
+	{
+		protected abstract TViewModel GetViewModel();
+
+		public BaseReactiveFragment() { }
+
+		protected BaseReactiveFragment(IntPtr handle, JniHandleOwnership ownership) : base(handle, ownership) { }
+
+		public override void OnActivityCreated(Android.OS.Bundle savedInstanceState)
+		{
+			base.OnActivityCreated(savedInstanceState);
+			ViewModel = GetViewModel();
+			OnViewModelSet();
+		}
+
+		protected virtual void OnViewModelSet()
+		{
+			SetViewModelBindings();
+		}
+
+		protected virtual void SetViewModelBindings()
+		{
+
+		}
+	}
+}
