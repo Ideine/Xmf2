f483fb449d35e7aa3a7d777d7484feac8be71f5c
Valentin Jubert <valentin.jubert@ideine.fr>
2017-11-21 15:56:50 +0100

    Ajout GetTopFragment

--- src/Xmf2.Commons.Droid/Extensions/FragmentExtensions.cs
+++ src/Xmf2.Commons.Droid/Extensions/FragmentExtensions.cs
@@ -1,20 +1,32 @@
-﻿using System;
+﻿using Android.Support.V4.App;
 using Android.Support.V7.App;
 
 public static class FragmentExtensions
 {
-	public static Android.Support.V4.App.FragmentManager GetSupportFragmentManager(this Android.App.Activity activity)
+	public static FragmentManager GetSupportFragmentManager(this Android.App.Activity activity)
 	{
 		return activity is AppCompatActivity appCompatActivity ? appCompatActivity.SupportFragmentManager : null;
 	}
 
-	public static TFragmentType FindFragmentByTag<TFragmentType>(this Android.Support.V4.App.FragmentManager fm, string tag)
-		where TFragmentType : Android.Support.V4.App.Fragment
+	public static Fragment GetTopFragment(this FragmentManager fm)
+	{
+		if (fm.Fragments.Count <= 0)
+		{
+			return null;
+		}
+		else
+		{
+			return fm.Fragments[fm.Fragments.Count - 1];
+		}
+	}
+
+	public static TFragmentType FindFragmentByTag<TFragmentType>(this FragmentManager fm, string tag)
+		where TFragmentType : Fragment
 	{
 		return fm.FindFragmentByTag(tag) as TFragmentType;
 	}
 
-	public static void ShowFragment(this AppCompatActivity activity, Android.Support.V4.App.Fragment fragment, int container, bool addToBackStack = false)
+	public static void ShowFragment(this AppCompatActivity activity, Fragment fragment, int container, bool addToBackStack = false)
 	{
 		var transaction = activity.SupportFragmentManager.BeginTransaction();
 
