87dbf38f7f52e015dc64788cd5d4219f484da967
DESKTOP-F04A74L\mcolo <mouhamadi.mcolomari@ideine.fr>
2017-11-29 18:13:01 +0100

    add BaseViewPresenter + WindowUIDispatcher

---
+++ src/Xmf2.Commons.UWP/WindowUIDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.UI.Core;
+using Xmf2.Commons.Services;
+
+namespace Xmf2.Commons.UWP
+{
+    public class WindowUIDispatcher : IUIDispatcher
+    {
+        private CoreDispatcher _dispatcher => Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
+
+        public void OnMainThread(Action action)
+        {
+            _dispatcher?.RunAsync(CoreDispatcherPriority.Normal, () => action?.Invoke());
+        }
+    }
+}
---
+++ src/Xmf2.Rx.UWP/Services/BaseViewPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Xmf2.Rx.UWP.Services
+{
+    public class BaseViewPresenter
+    {
+        public Frame CurrentPage => Window.Current.Content as Frame;
+       
+        public BaseViewPresenter() { }
+
+        public virtual void ShowView<IType>(object parameter = null)
+        {
+            ShowView(typeof(IType), parameter);
+        }
+
+        public virtual void ShowView(Type viewType, object parameter)
+        {
+            try
+            {
+                CurrentPage?.Navigate(viewType, parameter); //Frame won't allow serialization of it's nav-state if it gets a non-simple type as a nav param
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine($"Error seen during navigation request to {viewType.Name} - error {exception.ToString()}");
+            }
+        }
+    }
+}
