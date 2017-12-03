b42b8ae65fddaba95318b23cf178c41620ff0557
M'COLO MARI MOUHAMADI <mcolomari.mouhamadi@live.fr>
2017-12-03 18:35:34 +0100

    Add  LicenceReaderService

---
+++ src/Xmf2.Commons.UWP/Services/LicenceReaderService.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Windows.Storage;
+using Xmf2.Commons.Services.Licences;
+
+namespace Xmf2.Commons.UWP.Services
+{
+    public class LicenceReaderService : ILicenceReaderService
+    {
+        public string GetContent(string licencePathFile)
+        {
+            StorageFolder installationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+
+            string resourcePath = $"{installationFolder.Path }\\Assets\\{licencePathFile}";
+
+            using (Stream inputStream = File.OpenRead(resourcePath))
+            {
+                string content;
+
+                using (StreamReader sr = new StreamReader(inputStream))
+                {
+                    content = sr.ReadToEnd();
+                }
+                return content;
+            }
+
+        }
+    }
+}
