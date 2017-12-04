440e0d4bd7626f9e2b31cb3a6d3a515f4cfb648b
mcoutanceau <ma.coutanceau@ideine.fr>
2017-12-04 18:51:22 +0100

    [iOS] Breaking change: changed EnumExtensions namespace

--- src/Xmf2.Commons/Extensions/EnumExtensions.cs
+++ src/Xmf2.Commons/Extensions/EnumExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Xmf2.Commons.Extensions
+﻿namespace System
 {
 	public static class EnumExtensions
 	{
