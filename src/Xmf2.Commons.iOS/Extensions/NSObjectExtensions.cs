b7c429f3883c7d375b4c3794af65ba3c6dacadd8
mcoutanceau <ma.coutanceau@ideine.fr>
2017-12-13 11:08:22 +0100

    [iOS] Added NSObjectExtensions.ValueForKey(..., string key)

---
+++ src/Xmf2.Commons.iOS/Extensions/NSObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Foundation
+{
+	public static class NSObjectExtensions
+	{
+		public static NSObject ValueForKey(this NSObject nsObject, string key)
+		{
+			return nsObject.ValueForKey(new NSString(key));
+		}
+	}
+}
