cea47190da1f04fb5a065050768556f2535a8d76
Valentin Jubert <valentin.jubert@ideine.fr>
2018-01-24 13:44:24 +0100

    Add 10 Select

--- src/Xmf2.Rx/Extensions/IObservableExtensions.cs
+++ src/Xmf2.Rx/Extensions/IObservableExtensions.cs
@@ -38,15 +38,17 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2>(this IObservable<Tuple<T1, T2>> observable, Action<T1, T2> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
-			observable.Subscribe(tuple => 
+			observable.Subscribe(tuple =>
 			{
 				onNext(tuple.Item1, tuple.Item2);
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, T3>(this IObservable<Tuple<T1, T2, T3>> observable, Action<T1, T2, T3> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -56,6 +58,7 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, T3, T4>(this IObservable<Tuple<T1, T2, T3, T4>> observable, Action<T1, T2, T3, T4> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -65,6 +68,7 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, T3, T4, T5>(this IObservable<Tuple<T1, T2, T3, T4, T5>> observable, Action<T1, T2, T3, T4, T5> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -75,8 +79,7 @@ namespace System
 			}).DisposeWith(disposable);
 		}
 
-
-		public static void SubscribeAndDispose<T,R>(this IObservable<T> observable, Func<T, R> onNext)
+		public static void SubscribeAndDispose<T, R>(this IObservable<T> observable, Func<T, R> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
 			observable.Subscribe(x =>
@@ -85,6 +88,7 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, R>(this IObservable<Tuple<T1, T2>> observable, Func<T1, T2, R> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -94,6 +98,7 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, T3, R>(this IObservable<Tuple<T1, T2, T3>> observable, Func<T1, T2, T3, R> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -103,6 +108,7 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, T3, T4, R>(this IObservable<Tuple<T1, T2, T3, T4>> observable, Func<T1, T2, T3, T4, R> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -112,6 +118,7 @@ namespace System
 				disposable.Dispose();
 			}).DisposeWith(disposable);
 		}
+
 		public static void SubscribeAndDispose<T1, T2, T3, T4, T5, R>(this IObservable<Tuple<T1, T2, T3, T4, T5>> observable, Func<T1, T2, T3, T4, T5, R> onNext)
 		{
 			CompositeDisposable disposable = new CompositeDisposable();
@@ -137,7 +144,6 @@ namespace System
 			return errorHandler.Execute(observable, customHandler)
 				.Catch<T, Exception>(ex => Observable.Return(default(T)))
 				.Subscribe(onNext);
-
 		}
 
 		public static IObservable<T> ToObservableForBinding<T>(this IObservable<T> observable)
@@ -153,18 +159,22 @@ namespace System
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, TResult>(this IObservable<Tuple<T1, T2, T3>> source, Func<T1, T2, T3, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, T4, TResult>(this IObservable<Tuple<T1, T2, T3, T4>> source, Func<T1, T2, T3, T4, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, T4, T5, TResult>(this IObservable<Tuple<T1, T2, T3, T4, T5>> source, Func<T1, T2, T3, T4, T5, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, T4, T5, T6, TResult>(this IObservable<Tuple<T1, T2, T3, T4, T5, T6>> source, Func<T1, T2, T3, T4, T5, T6, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6));
@@ -179,48 +189,58 @@ namespace System
 
 		public static IObservable<T[]> AsArray<T>(this IObservable<Tuple<T, T>> source)
 		{
-			return source.Select(tuple => new T[] { tuple.Item1, tuple.Item2 });
+			return source.Select(tuple => new T[] {tuple.Item1, tuple.Item2});
 		}
+
 		public static IObservable<T[]> AsArray<T>(this IObservable<Tuple<T, T, T>> source)
 		{
-			return source.Select(tuple => new T[] { tuple.Item1, tuple.Item2, tuple.Item3 });
+			return source.Select(tuple => new T[] {tuple.Item1, tuple.Item2, tuple.Item3});
 		}
+
 		public static IObservable<T[]> AsArray<T>(this IObservable<Tuple<T, T, T, T>> source)
 		{
-			return source.Select(tuple => new T[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4 });
+			return source.Select(tuple => new T[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4});
 		}
+
 		public static IObservable<T[]> AsArray<T>(this IObservable<Tuple<T, T, T, T, T>> source)
 		{
-			return source.Select(tuple => new T[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5});
+			return source.Select(tuple => new T[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5});
 		}
+
 		public static IObservable<T[]> AsArray<T>(this IObservable<Tuple<T, T, T, T, T, T>> source)
 		{
-			return source.Select(tuple => new T[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6 });
+			return source.Select(tuple => new T[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6});
 		}
+
 		public static IObservable<T[]> AsArray<T>(this IObservable<Tuple<T, T, T, T, T, T, T>> source)
 		{
-			return source.Select(tuple => new T[] { tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7 });
+			return source.Select(tuple => new T[] {tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7});
 		}
+
 		#endregion AsArray
 
 		#region Tuple Subscribe
+
 		public static IDisposable Subscribe<T1, T2>(this IObservable<Tuple<T1, T2>> source, Action<T1, T2> onNext)
 		{
 			return source.Select(NullToTupleWithDefault).Subscribe(tuple => onNext(tuple.Item1, tuple.Item2));
 		}
+
 		public static IDisposable Subscribe<T1, T2, T3>(this IObservable<Tuple<T1, T2, T3>> source, Action<T1, T2, T3> onNext)
 		{
 			return source.Select(NullToTupleWithDefault).Subscribe(tuple => onNext(tuple.Item1, tuple.Item2, tuple.Item3));
 		}
+
 		public static IDisposable Subscribe<T1, T2, T3, T4>(this IObservable<Tuple<T1, T2, T3, T4>> source, Action<T1, T2, T3, T4> onNext)
 		{
 			return source.Select(NullToTupleWithDefault).Subscribe(tuple => onNext(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4));
 		}
+
 		public static IDisposable Subscribe<T1, T2, T3, T4, T5>(this IObservable<Tuple<T1, T2, T3, T4, T5>> source, Action<T1, T2, T3, T4, T5> onNext)
 		{
 			return source.Select(NullToTupleWithDefault).Subscribe(tuple => onNext(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5));
 		}
-		
+
 		private static Tuple<T1, T2> NullToTupleWithDefault<T1, T2>(Tuple<T1, T2> tuple) => tuple ?? new Tuple<T1, T2>(default(T1), default(T2));
 		private static Tuple<T1, T2, T3> NullToTupleWithDefault<T1, T2, T3>(Tuple<T1, T2, T3> tuple) => tuple ?? new Tuple<T1, T2, T3>(default(T1), default(T2), default(T3));
 		private static Tuple<T1, T2, T3, T4> NullToTupleWithDefault<T1, T2, T3, T4>(Tuple<T1, T2, T3, T4> tuple) => tuple ?? new Tuple<T1, T2, T3, T4>(default(T1), default(T2), default(T3), default(T4));
@@ -229,51 +249,68 @@ namespace System
 		#endregion
 
 		#region ValueTuple Subscribe
+
 		public static IDisposable Subscribe<T1, T2>(this IObservable<(T1, T2)> source, Action<T1, T2> onNext)
 		{
 			return source.Subscribe(tuple => onNext(tuple.Item1, tuple.Item2));
 		}
+
 		public static IDisposable Subscribe<T1, T2, T3>(this IObservable<(T1, T2, T3)> source, Action<T1, T2, T3> onNext)
 		{
 			return source.Subscribe(tuple => onNext(tuple.Item1, tuple.Item2, tuple.Item3));
 		}
+
 		public static IDisposable Subscribe<T1, T2, T3, T4>(this IObservable<(T1, T2, T3, T4)> source, Action<T1, T2, T3, T4> onNext)
 		{
 			return source.Subscribe(tuple => onNext(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4));
 		}
+
 		public static IDisposable Subscribe<T1, T2, T3, T4, T5>(this IObservable<(T1, T2, T3, T4, T5)> source, Action<T1, T2, T3, T4, T5> onNext)
 		{
 			return source.Subscribe(tuple => onNext(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5));
 		}
+
 		#endregion
 
 		#region ValueTuple Select
+
 		public static IObservable<TResult> Select<T1, T2, TResult>(this IObservable<(T1, T2)> source, Func<T1, T2, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, TResult>(this IObservable<(T1, T2, T3)> source, Func<T1, T2, T3, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, T4, TResult>(this IObservable<(T1, T2, T3, T4)> source, Func<T1, T2, T3, T4, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, T4, T5, TResult>(this IObservable<(T1, T2, T3, T4, T5)> source, Func<T1, T2, T3, T4, T5, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5));
 		}
+
 		public static IObservable<TResult> Select<T1, T2, T3, T4, T5, T6, TResult>(this IObservable<(T1, T2, T3, T4, T5, T6)> source, Func<T1, T2, T3, T4, T5, T6, TResult> selector)
 		{
 			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6));
 		}
+
+		public static IObservable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this IObservable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> source, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> selector)
+		{
+			return source.Select(tuple => selector(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Item8, tuple.Item9, tuple.Item10));
+		}
+
 		#endregion
 
 		public static IObservable<T> TriggeredAlsoOn<T, T1>(this IObservable<T> mainObservable, IObservable<T1> observable1)
 		{
 			return Observable.CombineLatest(mainObservable, observable1, (mainObserved, _) => mainObserved);
 		}
+
 		public static IObservable<T> TriggeredAlsoOn<T, T1, T2>(
 			this IObservable<T> mainObservable,
 			IObservable<T1> observable1,
@@ -281,6 +318,7 @@ namespace System
 		{
 			return Observable.CombineLatest(mainObservable, observable1, observable2, (mainObserved, _1, _2) => mainObserved);
 		}
+
 		public static IObservable<T> TriggeredAlsoOn<T, T2, T3, T4>(
 			this IObservable<T> mainObservable,
 			IObservable<T2> observable1,
