3cd7e7d4a5a795633cc7cbfe7ec36a127db8187e
DESKTOP-F04A74L\mcolo <mouhamadi.mcolomari@ideine.fr>
2018-02-05 11:35:04 +0100

    Add HockeyApp reporting

---
+++ src/Xmf2.Rx.UWP/Services/Reportings/HockeyAppService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.HockeyApp;
+using Xmf2.Rx.Services.Reportings;
+
+namespace Xmf2.Rx.UWP.Services.Reportings
+{
+    public class HockeyAppService : IHockeyAppService
+    {
+        private readonly string _appId;
+
+        private IHockeyClient _manager;
+
+        public HockeyAppService(string appId)
+        {
+            _manager = Microsoft.HockeyApp.HockeyClient.Current;
+        }
+
+        public void EnableCrashReporting()
+        {
+            _manager.Configure(_appId,new TelemetryConfiguration() { EnableDiagnostics = true});
+        }
+
+        public void DisableCrashReporting()
+        {
+            _manager.Configure(_appId, new TelemetryConfiguration() { EnableDiagnostics = false });
+        }
+    }
+}
