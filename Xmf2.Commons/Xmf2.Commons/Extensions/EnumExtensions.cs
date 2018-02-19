3c3e02d7192b34deed903254ef35b99968f40bf6
mcoutanceau <ma.coutanceau@ideine.fr>
2018-02-19 15:45:28 +0100

    [Common] Added EnumExtensions

---
+++ Xmf2.Commons/Xmf2.Commons/Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace System
+{
+	public static class EnumExtensions
+	{
+		public static NotSupportedException GetNotSupportedException(this Enum enumValue)
+		{
+			return new NotSupportedException(GetNotSupportedMessage(enumValue));
+		}
+		public static string GetNotSupportedMessage(this Enum enumValue)
+		{
+			return string.Format("This {0} value is not supported yet. Value: {1}"
+								, enumValue.GetType().FullName
+								, enumValue);
+		}
+	}
+}
