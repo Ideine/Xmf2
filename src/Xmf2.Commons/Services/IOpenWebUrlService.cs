3e925f1d960d7f3856a5b788a1564508dcb6b7f7
Valentin Jubert <valentin.jubert@ideine.fr>
2018-02-26 17:34:21 +0100

    OpenUrl

---
+++ src/Xmf2.Commons/Services/IOpenWebUrlService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Xmf2.Commons.Services
+{
+	public interface IOpenWebUrlService
+	{
+		void OpenWebsite(string url);
+	}
+}
---
+++ src/Xmf2.Rx.Droid/Services/OpenWebUrlService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.Content;
+using Xmf2.Commons.Droid.Services;
+using Xmf2.Commons.Services;
+using Xmf2.Rx.Helpers;
+
+namespace Xmf2.Rx.Droid.Services
+{
+	public class OpenWebUrlService: IOpenWebUrlService
+	{
+		private readonly LazyLocatorOf<ICurrentActivity> _currentActivty = new LazyLocatorOf<ICurrentActivity>();
+
+		public void OpenWebsite(string url)
+		{
+			var intent = new Intent(Intent.ActionView);
+			intent.SetData(Android.Net.Uri.Parse(url));
+			_currentActivty.Value.Activity.StartActivity(intent);
+		}
+	}
+}
