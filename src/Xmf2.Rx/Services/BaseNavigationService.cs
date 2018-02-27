8058e4c292630bc016d12e33a8686b8332b43ce7
Valentin Jubert <valentin.jubert@ideine.fr>
2018-02-27 10:16:23 +0100

    Base ViewPresenter & Base Navigation Service

---
+++ src/Xmf2.Rx/Services/BaseNavigationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xmf2.Rx.ViewModels;
+
+namespace Xmf2.Rx.Services
+{
+	public abstract class BaseNavigationService
+	{
+		protected abstract IBaseViewPresenterService BasePresenter { get; }
+
+		protected virtual async Task<TViewModel> Initialize<TViewModel>(TViewModel viewModel)
+			where TViewModel : BaseViewModel
+		{
+			viewModel.LifecycleManager.Initialize();
+			await viewModel.LifecycleManager.WaitForInitialization().ConfigureAwait(false);
+			return viewModel;
+		}
+
+		public virtual async Task Close<TViewModel>(TViewModel viewModel)
+			where TViewModel : BaseViewModel
+		{
+			if (await BasePresenter.IsCurrentViewModel(viewModel))
+			{
+				BasePresenter.Close();
+			}
+			else
+			{
+				throw new Exception("Trying to close a ViewModel but it's not the top of the stack !");
+			}
+		}
+	}
+}
---
+++ src/Xmf2.Rx/Services/IBaseViewPresenterService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xmf2.Rx.ViewModels;
+
+namespace Xmf2.Rx.Services
+{
+	public interface IBaseViewPresenterService
+	{
+		Task<bool> IsCurrentViewModel<TViewModel>(TViewModel viewModel) where TViewModel : BaseViewModel;
+		void Close();
+	}
+}
