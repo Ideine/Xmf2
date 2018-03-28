bb88a6e999a1236b8a755ad24a16034757989588
mcoutanceau <ma.coutanceau@ideine.fr>
2018-03-28 16:45:02 +0200

    Added EnumerableExtensions.Append(...)

--- Xmf2.Commons/Xmf2.Commons/Extensions/EnumerableExtensions.cs
+++ Xmf2.Commons/Xmf2.Commons/Extensions/EnumerableExtensions.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Xmf2.Commons.Extensions
 {
-    public static class EnumerableExtensions
+	public static class EnumerableExtensions
     {
         public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> lst)
         {
@@ -16,5 +13,11 @@ namespace Xmf2.Commons.Extensions
                 return lst;
         }
 
+		public static IEnumerable<T> Append<T>(this IEnumerable<T> lst, T obj)
+		{
+			foreach (var o in lst)
+				yield return o;
+			yield return obj;
+		}
     }
 }
