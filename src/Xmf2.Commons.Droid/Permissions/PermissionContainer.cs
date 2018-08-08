1bd90d1302a5454c1596c96ec4c22189ce121652
mouhamadi <mouhamadi.mcolomari@ideine.fr>
2018-08-08 15:23:27 +0200

    Added PermissionContainer

---
+++ src/Xmf2.Commons.Droid/Permissions/PermissionContainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.Content.PM;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Xmf2.Commons.Droid.Permissions
+{
+	public static class PermissionContainer
+	{
+		private static readonly Dictionary<int, TaskCompletionSource<Permission[]>> _permissions = new Dictionary<int, TaskCompletionSource<Permission[]>>();
+
+		public static Task<Permission[]> WaitForPermission(int code)
+		{
+			var result = new TaskCompletionSource<Permission[]>();
+
+			if (_permissions.TryAdd(code, result))
+			{
+				return result.Task;
+			}
+			if (_permissions.TryGetValue(code, out result))
+			{
+				return result.Task;
+			}
+			return null;
+		}
+
+		public static void OnResult(int requestCode, Permission[] grantResults)
+		{
+			if (_permissions.TryGetValue(requestCode, out var result))
+			{
+				result.TrySetResult(grantResults);
+				_permissions.Remove(requestCode);
+			}
+		}
+	}
+}
