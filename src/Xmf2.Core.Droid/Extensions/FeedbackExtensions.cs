7a1e591e60477bec62a73f5c8aac2f2085f54ead
francois <francois.bernard@ideine.fr>
2019-03-12 16:35:39 +0100

    added ResizeViewHelper and updated FeedbackExtensions

--- src/Xmf2.Core.Droid/Extensions/FeedbackExtensions.cs
+++ src/Xmf2.Core.Droid/Extensions/FeedbackExtensions.cs
@@ -7,83 +7,84 @@ using Android.Graphics.Drawables;
 
 namespace Xmf2.Core.Droid.Extensions
 {
-	public static class FeedbackExtensions
-	{
-		#region Elevation highlight
+    public static class FeedbackExtensions
+    {
+        #region Elevation highlight
 
-		public static T WithElevationHighlight<T>(this T view, Xmf2Disposable disposer, int highlightElevationInDp = 4) where T : View
-		{
-			var highlightElevation = UIHelper.DpToPx(view.Context, highlightElevationInDp);
+        public static T WithElevationHighlight<T>(this T view, Xmf2Disposable disposer, int highlightElevationInDp = 4) where T : View
+        {
+            var highlightElevation = UIHelper.DpToPx(view.Context, highlightElevationInDp);
 
-			view.Elevation = highlightElevation;
+            view.Elevation = highlightElevation;
 
-			new EventSubscriber<View>(
-				view,
-				btn => btn.SetOnTouchListener(new TouchViewListener<View>(btn).DisposeWith(disposer).WithTouchHighlight(FromHighlight, ToHighlight)),
-				btn => btn.SetOnTouchListener(null)
-			).DisposeEventWith(disposer);
+            new EventSubscriber<View>(
+                view,
+                btn => btn.SetOnTouchListener(new TouchViewListener<View>(btn).DisposeWith(disposer).WithTouchHighlight(FromHighlight, ToHighlight)),
+                btn => btn.SetOnTouchListener(null)
+            ).DisposeEventWith(disposer);
 
-			void ToHighlight(View highlightView)
-			{
-				highlightView.ScaleX = highlightView.ScaleY = 0.95f;
-				SetElevation(highlightView, 0);
-			}
+            void ToHighlight(View highlightView)
+            {
+                highlightView.ScaleX = highlightView.ScaleY = 0.95f;
+                SetElevation(highlightView, 0);
+            }
 
-			void FromHighlight(View highlightView)
-			{
-				highlightView.ScaleX = highlightView.ScaleY = 1f;
-				SetElevation(highlightView, highlightElevation);
-			}
+            void FromHighlight(View highlightView)
+            {
+                highlightView.ScaleX = highlightView.ScaleY = 1f;
+                SetElevation(highlightView, highlightElevation);
+            }
 
-			void SetElevation(View elevationView, int value)
-			{
-				if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
-				{
-					elevationView.Elevation = value;
-				}
-			}
+            void SetElevation(View elevationView, int value)
+            {
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                {
+                    elevationView.Elevation = value;
+                }
+            }
 
-			return view;
-		}
+            return view;
+        }
 
-		#endregion
+        #endregion
 
-		#region Background highlight 
+        #region Background highlight 
 
-		public static T WithFadeHighlight<T>(this T view, Xmf2Disposable disposer) where T : View
-		{
-			new EventSubscriber<View>(
-				view,
-				btn => btn.SetOnTouchListener(new TouchViewListener<View>(btn).DisposeWith(disposer).WithTouchHighlight(FromFadeHighlight, ToFadeHighlight)),
-				btn => btn.SetOnTouchListener(null)
-			).DisposeEventWith(disposer);
+        public static T WithFadeHighlight<T>(this T view, Xmf2Disposable disposer) where T : View
+        {
+            new EventSubscriber<View>(
+                view,
+                btn => btn.SetOnTouchListener(new TouchViewListener<View>(btn).DisposeWith(disposer).WithTouchHighlight(FromFadeHighlight, ToFadeHighlight)),
+                btn => btn.SetOnTouchListener(null)
+            ).DisposeEventWith(disposer);
 
-			void ToFadeHighlight(View highlightView) => highlightView.Alpha = 0.58f;
-			void FromFadeHighlight(View highlightView) => highlightView.Alpha = 1f;
+            void ToFadeHighlight(View highlightView) => highlightView.Alpha = 0.58f;
+            void FromFadeHighlight(View highlightView) => highlightView.Alpha = 1f;
 
-			return view;
-		}
+            return view;
+        }
 
-		public static void SetBackgroundWithHighlight(this View view, Color backgroundColor, Color highlightColor, Xmf2Disposable disposer)
-		{
-			var st = new StateListDrawable();
+        public static void SetBackgroundWithHighlight(this View view, Color backgroundColor, Color highlightColor, Xmf2Disposable disposer)
+        {
+            var st = new StateListDrawable();
 
-			st.AddState(new int[] { Android.Resource.Attribute.StatePressed }, new ColorDrawable(highlightColor).DisposeWith(disposer));
-			st.AddState(new int[] { }, new ColorDrawable(backgroundColor).DisposeWith(disposer));
+            st.AddState(new int[] { Android.Resource.Attribute.StatePressed }, new ColorDrawable(highlightColor).DisposeWith(disposer));
+            st.AddState(new int[] { }, new ColorDrawable(backgroundColor).DisposeWith(disposer));
 
-			view.Background = st;
-		}
+            view.Background = st;
+        }
 
-		public static void SetBackgroundWithHighlight(this View view, Drawable background, Drawable highlight)
-		{
-			var st = new StateListDrawable();
+        public static void SetBackgroundWithHighlight(this View view, Drawable background, Drawable highlight, Drawable disabled = null)
+        {
+            var st = new StateListDrawable();
 
-			st.AddState(new int[] { Android.Resource.Attribute.StatePressed }, highlight);
-			st.AddState(new int[] { }, background);
+            st.AddState(new int[] { Android.Resource.Attribute.StatePressed }, highlight);
+            st.AddState(new int[] { -Android.Resource.Attribute.StateEnabled }, disabled);
+            st.AddState(new int[] { }, background);
 
-			view.Background = st;
-		}
+            view.Background = st;
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
---
+++ src/Xmf2.Core.Droid/Helpers/ResizeViewHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.Graphics;
+using Android.Runtime;
+
+namespace Xmf2.Core.Droid.Helpers
+{
+    public class ResizeViewHelper : Java.Lang.Object, ViewTreeObserver.IOnGlobalLayoutListener, IDisposable
+    {
+        private View _rootView;
+        private View _target;
+        private Action<bool> _onKeyBoardVisibilityChanged;
+
+        protected ResizeViewHelper(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+
+        public ResizeViewHelper(Activity activity, View target, Action<bool> onKeyBoardVisible = null)
+        {
+            _rootView = activity.FindViewById(Android.Resource.Id.Content);
+            _target = target;
+            _onKeyBoardVisibilityChanged = onKeyBoardVisible;
+            _rootView?.ViewTreeObserver?.AddOnGlobalLayoutListener(this);
+        }
+
+        public void OnGlobalLayout()
+        {
+            using (var r = new Rect())
+            {
+                try
+                {
+                    _rootView.GetWindowVisibleDisplayFrame(r);
+
+                    int screenHeight = _rootView.Height;
+                    float keyboardHeight = (float)(_rootView.Height - r.Bottom);
+                    OnKeyboardVisibilityChanged(r.Bottom, keyboardHeight, keyboardHeight > 0);
+                }
+                catch (Exception ex)
+                {
+                    //view can be disposed
+                }
+            }
+        }
+
+        private void OnKeyboardVisibilityChanged(float visibleScreenHeight, float keyboardHeight, bool visible)
+        {
+            if (_target.LayoutParameters is ViewGroup.MarginLayoutParams lp)
+            {
+                int targetHeight = visible ? (int)visibleScreenHeight : ViewGroup.MarginLayoutParams.MatchParent;
+                if (lp.Height != targetHeight)
+                {
+                    lp.Height = targetHeight;
+                    _target.LayoutParameters = lp;
+                }
+            }
+            _onKeyBoardVisibilityChanged?.Invoke(visible);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _rootView?.ViewTreeObserver?.RemoveOnGlobalLayoutListener(this);
+
+                _rootView?.Dispose();
+                _rootView = null;
+                _onKeyBoardVisibilityChanged = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
