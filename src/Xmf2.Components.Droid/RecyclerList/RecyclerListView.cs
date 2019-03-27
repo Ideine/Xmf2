cd927c3ebf6f6171e2a8283140a8f49bb2a6a8ff
mouhamadi <mouhamadi.mcolomari@ideine.fr>
2019-03-27 13:11:11 +0100

    Add RecyclerItemsView + RecyclerListView + RecyclerGridView

---
+++ src/Xmf2.Components.Droid/RecyclerList/RecyclerListView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xmf2.Core.Subscriptions;
+using Android.Support.V7.Widget;
+using Xmf2.Components.Interfaces;
+using Xmf2.Components.Droid.Interfaces;
+
+namespace Xmf2.Components.Droid.RecyclerList
+{
+	public class RecyclerListView : RecyclerItemsView
+	{
+		public virtual int Orientation => LinearLayoutManager.Vertical;
+
+		public RecyclerListView(IServiceLocator services, Func<IServiceLocator, IComponentView> factory) : base(services, factory) { }
+
+		protected override void SetLayoutManager()
+		{
+			using (var lm = new LinearLayoutManager(Context, Orientation, reverseLayout: false).DisposeWith(Disposables))
+			{
+				RecyclerView.SetLayoutManager(lm);
+			}
+		}
+	}
+}
