dd44761f2123605d9f9fcef5a7b3aa3286601643
mouhamadi <mouhamadi.mcolomari@ideine.fr>
2019-03-27 14:23:12 +0100

    review CreateDrawable

--- src/Xmf2.Core.Droid/Extensions/BackgroundExtensions.cs
+++ src/Xmf2.Core.Droid/Extensions/BackgroundExtensions.cs
@@ -1,18 +1,21 @@
-﻿using Android.Views;
-using Android.Graphics;
-using Xmf2.Core.Droid.Helpers;
+﻿using Android.Graphics;
 using Android.Graphics.Drawables;
 
 namespace Xmf2.Core.Droid.Extensions
 {
 	public static class BackgroundExtensions
 	{
-		public static Drawable CreateDrawable(this object _, Color color, int? cornerRadiusInPx = 0, Color? strokeColor = null, int stokeWidthInPx = 0, int dashedWidthInPx = 0, int dashGap = 0)
+		public static Drawable CreateDrawable(this object _, Color color, int? cornerRadiusInPx = 0, Color? strokeColor = null, int stokeWidthInPx = 0, int dashedWidthInPx = 0, int dashGap = 0, int? width = null, int? height = null)
 		{
 			var bg = new GradientDrawable();
 			bg.SetShape(ShapeType.Rectangle);
 			bg.SetOrientation(GradientDrawable.Orientation.LeftRight);
 			bg.SetColors(new int[] { color, color });
+			if (width.HasValue && height.HasValue)
+			{
+				bg.SetSize(width.Value, height.Value);
+			}
+
 			if (cornerRadiusInPx.HasValue)
 			{
 				bg.SetCornerRadius(cornerRadiusInPx.Value);
@@ -23,26 +26,5 @@ namespace Xmf2.Core.Droid.Extensions
 			}
 			return bg;
 		}
-
-		public static void SetGradientBackground(this View view, Color leftColor, Color righColor, int? cornerRadius = null, Color? stokeColor = null, int stokeWidth = 0)
-		{
-			using (var bg = new GradientDrawable())
-			{
-				bg.SetShape(ShapeType.Rectangle);
-				bg.SetOrientation(GradientDrawable.Orientation.TopBottom);
-				bg.SetColors(new int[] { leftColor, righColor });
-				if (cornerRadius.HasValue)
-				{
-					bg.SetCornerRadius(UIHelper.DpToPx(view.Context, cornerRadius.Value));
-				}
-
-				if (stokeColor.HasValue)
-				{
-					bg.SetStroke(UIHelper.DpToPx(view.Context, stokeWidth), stokeColor.Value);
-				}
-				view.Background = bg;
-			}
-		}
-
 	}
 }
