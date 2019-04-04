ada34ca12004990b925061b9bc66ed73b7dac111
ma.coutanceau <ma.coutanceau@ideine.fr>
2019-04-04 16:31:09 +0200

    Added UIDisposableView

---
+++ src/Xmf2.Core.iOS/Controls/UIDisposableView.cs
@@ -0,0 +1,24 @@
+﻿using UIKit;
+using Xmf2.Core.Subscriptions;
+
+namespace Xmf2.Core.iOS.Controls
+{
+	public class UIDisposableView : UIView
+	{
+		protected Xmf2Disposable Disposable { get; }
+
+		public UIDisposableView()
+		{
+			Disposable = new Xmf2Disposable();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				Disposable.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
