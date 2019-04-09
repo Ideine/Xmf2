9de9639d128d070b5143b674c2346b0ade5c9dd0
ma.coutanceau <ma.coutanceau@ideine.fr>
2019-04-09 15:03:24 +0200

    Deleted classes related to Xmf2.NavigationGraph

--- src/Xmf2.Components/Navigations/ViewModelCreator.cs
+++
@@ -1,7 +0,0 @@
-using System.Threading.Tasks;
-using Xmf2.Components.Interfaces;
-
-namespace Xmf2.Components.Navigations
-{
-	public delegate Task<IComponentViewModel> ViewModelCreator(string route);
-}
