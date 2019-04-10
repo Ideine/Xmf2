fea4e7b3dce12f40f3a34a7d70675031442a17e5
ma.coutanceau <ma.coutanceau@ideine.fr>
2019-04-10 14:25:21 +0200

    Fixed UIColumn view update bug

--- src/Xmf2.Core.iOS/Controls/UIColumnView.cs
+++ src/Xmf2.Core.iOS/Controls/UIColumnView.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UIKit;
 using static UIKit.NSLayoutAttribute;
@@ -10,47 +9,44 @@ namespace Xmf2.Core.iOS.Controls
 	public class UIColumnView : UIView
 	{
 		private List<SubviewConstraints> _subviewConstraints;
-		private UIView[] _subviews;
 
 		public UIColumnView()
 		{
 			_subviewConstraints = new List<SubviewConstraints>();
-			_subviews = new UIView[0];
 		}
-
 
 		public void SetSubviews(UIView[] subviews)
 		{
-			this.EnsureRemove(this.Subviews);
-			this.AddSubviews(subviews);
-			this.SetNeedsUpdateConstraints();
+			if (SubviewsAreDifferent(subviews, this.Subviews))
+			{
+				this.EnsureRemove(this.Subviews);
+				this.AddSubviews(subviews);
+				this.SetNeedsUpdateConstraints();
+			}
 		}
 
 		public override void UpdateConstraints()
 		{
 			base.UpdateConstraints();
 
-			if (SubviewsAreDifferent(_subviews, this.Subviews))
+			var subviews = this.Subviews.ToArray();
+
+			this.EnsureRemove(_subviewConstraints.SelectMany(o => o.AllConstraints()));
+			_subviewConstraints.Clear();
+
+			for (int i = 0; i < subviews.Length; i++)
 			{
-				_subviews = this.Subviews.ToArray();
-
-				this.EnsureRemove(_subviewConstraints.SelectMany(o => o.AllConstraints()));
-				_subviewConstraints.Clear();
-
-				for (int i = 0; i < _subviews.Length; i++)
+				UIView subView = subviews[i];
+				subView.TranslatesAutoresizingMaskIntoConstraints = false;
+				float multiplier = ((i * 2f) + 1f) / subviews.Length;
+				var newConstraints = new SubviewConstraints
 				{
-					UIView subView = _subviews[i];
-					subView.TranslatesAutoresizingMaskIntoConstraints = false;
-					float multiplier = ((i * 2f) + 1f) / _subviews.Length;
-					var newConstraints = new SubviewConstraints
-					{
-						HCenterX = NSLayoutConstraint.Create(subView, CenterX, Equal, this, CenterX, multiplier, 0f).WithAutomaticIdentifier(),
-						VCenterY = NSLayoutConstraint.Create(subView, CenterY, Equal, this, CenterY, 1f, 0f).WithAutomaticIdentifier(),
-						VMaxHeight = NSLayoutConstraint.Create(subView, Height, LessThanOrEqual, this, Height, 1f, 0f).WithAutomaticIdentifier()
-					};
-					_subviewConstraints.Add(newConstraints);
-					this.EnsureAdd(newConstraints.AllConstraints());
-				}
+					HCenterX = NSLayoutConstraint.Create(subView, CenterX, Equal, this, CenterX, multiplier, 0f).WithAutomaticIdentifier(),
+					VCenterY = NSLayoutConstraint.Create(subView, CenterY, Equal, this, CenterY, 1f, 0f).WithAutomaticIdentifier(),
+					VMaxHeight = NSLayoutConstraint.Create(subView, Height, LessThanOrEqual, this, Height, 1f, 0f).WithAutomaticIdentifier()
+				};
+				_subviewConstraints.Add(newConstraints);
+				this.EnsureAdd(newConstraints.AllConstraints());
 			}
 		}
 
@@ -94,7 +90,6 @@ namespace Xmf2.Core.iOS.Controls
 					_subviewConstraints.Clear();
 				}
 				_subviewConstraints = null;
-				_subviews = null;
 			}
 			base.Dispose(disposing);
 		}
