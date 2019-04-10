3c6245236fe0b08486624a2cc9695dfd212eb353
ma.coutanceau <ma.coutanceau@ideine.fr>
2019-04-10 14:25:50 +0200

    Updated ByCaseView to allow lazy behavior

--- src/Xmf2.Components.iOS/Views/Multistates/ByCaseView.cs
+++ src/Xmf2.Components.iOS/Views/Multistates/ByCaseView.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UIKit;
 using Xmf2.Components.Interfaces;
@@ -11,19 +12,25 @@ namespace Xmf2.Components.iOS.Views.Multistates
 {
 	public class ByCaseView<TCaseEnum> : BaseComponentView<ByCaseViewState<TCaseEnum>>
 	{
-		private Dictionary<TCaseEnum, IComponentView> _componentByCase;
+		private Dictionary<TCaseEnum, ComponentInfo> _byCaseInfo;
 		private UIByCaseView<TCaseEnum> _uiByCaseView;
+		private ComponentInfo _currentInfo;
+		private bool _aggressiveViewDispose;
 
-		public ByCaseView(IServiceLocator services, Dictionary<TCaseEnum, IComponentView> componentByCase) : base(services)
+		public ByCaseView(IServiceLocator services, Dictionary<TCaseEnum, Func<IComponentView>> componentFactoryByCase) : base(services)
 		{
-			_componentByCase = componentByCase;
-
-			var viewByCase = new Dictionary<TCaseEnum, UIView>(
-				collection: componentByCase.Select(kvp => new KeyValuePair<TCaseEnum, UIView>(kvp.Key, kvp.Value.View)),
-				comparer: componentByCase.Comparer
+			_byCaseInfo = componentFactoryByCase.ToDictionary(
+				keySelector: kvp => kvp.Key,
+				elementSelector: kvp => new ComponentInfo { ComponentFactory = kvp.Value }.DisposeWith(Disposables),
+				comparer: componentFactoryByCase.Comparer
 			);
 
-			_uiByCaseView = new UIByCaseView<TCaseEnum>(viewByCase).DisposeViewWith(Disposables);
+			Dictionary<TCaseEnum, Func<UIView>> viewFactoryByCase = _byCaseInfo.ToDictionary(
+				keySelector: kvp => kvp.Key,
+				elementSelector: kvp => kvp.Value.GetViewFactory(),
+				comparer: componentFactoryByCase.Comparer
+			);
+			_uiByCaseView = new UIByCaseView<TCaseEnum>(viewFactoryByCase).DisposeViewWith(Disposables);
 		}
 
 		protected override UIView RenderView() => _uiByCaseView;
@@ -31,18 +38,69 @@ namespace Xmf2.Components.iOS.Views.Multistates
 		protected override void OnStateUpdate(ByCaseViewState<TCaseEnum> state)
 		{
 			base.OnStateUpdate(state);
-			_componentByCase[state.Case].SetState(state.State);
+
+			ComponentInfo newInfo = _byCaseInfo[state.Case];
+			_byCaseInfo[state.Case].GetComponent().SetState(state.State);
 			_uiByCaseView.WithCase(state.Case);
+
+			if (   newInfo != _currentInfo
+				&& _aggressiveViewDispose)
+			{
+				_currentInfo?.DisposeComponent();
+			}
+			_currentInfo = newInfo;
+		}
+
+		public ByCaseView<TCaseEnum> WithAggressiveViewDispose(bool aggressive = true)
+		{
+			_uiByCaseView.WithAggressiveViewDispose(aggressive);
+			_aggressiveViewDispose = aggressive;
+			return this;
 		}
 
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing)
 			{
-				_componentByCase = null;
+				_byCaseInfo = null;
 				_uiByCaseView = null;
 			}
 			base.Dispose(disposing);
 		}
+
+		#region Nested Types
+
+		private class ComponentInfo : IDisposable
+		{
+			private bool _disposedValue = false;
+			private IComponentView _component;
+
+			public Func<IComponentView> ComponentFactory { private get; set; }
+
+			public IComponentView GetComponent() => _component ?? (_component = ComponentFactory());
+			public Func<UIView> GetViewFactory() => () => GetComponent().View;
+
+			internal void DisposeComponent()
+			{
+				_component?.Dispose();
+				_component = null;
+			}
+
+			public void Dispose() => Dispose(true);
+			protected virtual void Dispose(bool disposing)
+			{
+				if (!_disposedValue)
+				{
+					if (disposing)
+					{
+						ComponentFactory = null;
+						DisposeComponent();
+					}
+					_disposedValue = true;
+				}
+			}
+		}
+
+		#endregion Nested Types
 	}
 }
--- src/Xmf2.Core.iOS/Controls/UIByCaseView.cs
+++ src/Xmf2.Core.iOS/Controls/UIByCaseView.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UIKit;
 using Xmf2.Core.Subscriptions;
@@ -11,41 +12,26 @@ namespace Xmf2.Core.iOS.Controls
 
 		private bool _currentCaseOnceSet = false;
 		private TCaseEnum _currentCase;
-		private Dictionary<TCaseEnum, UIView> _viewByCase;
-		private Dictionary<UIView, NSLayoutConstraint[]> _constraintsByView;
+		private ViewInfo _currentInfo;
+		private Dictionary<TCaseEnum, ViewInfo> _byCaseInfo;
+		private bool _aggressiveViewDispose = false;
 
-		public UIByCaseView(Dictionary<TCaseEnum, UIView> viewByCase)
+		public UIByCaseView(Dictionary<TCaseEnum, Func<UIView>> viewFactoryByCase)
 		{
 			_disposable = new Xmf2Disposable();
 			_currentCase = default(TCaseEnum);
-			_viewByCase = viewByCase;
-			_constraintsByView = new Dictionary<UIView, NSLayoutConstraint[]>();
-		}
 
-		private NSLayoutConstraint[] GetContraintsFor(UIView view)
-		{
-			if (_constraintsByView.TryGetValue(view, out var constraints))
-			{
-				return constraints;
-			}
-			else
-			{
-				var newConstraints = new NSLayoutConstraint[]
-				{
-					NSLayoutConstraint.Create(this, NSLayoutAttribute.CenterX , NSLayoutRelation.Equal, view, NSLayoutAttribute.CenterX, 1f, 0f).WithAutomaticIdentifier().DisposeWith(_disposable),
-					NSLayoutConstraint.Create(this, NSLayoutAttribute.CenterY , NSLayoutRelation.Equal, view, NSLayoutAttribute.CenterY, 1f, 0f).WithAutomaticIdentifier().DisposeWith(_disposable),
-					NSLayoutConstraint.Create(this, NSLayoutAttribute.Width   , NSLayoutRelation.Equal, view, NSLayoutAttribute.Width  , 1f, 0f).WithAutomaticIdentifier().DisposeWith(_disposable),
-					NSLayoutConstraint.Create(this, NSLayoutAttribute.Height  , NSLayoutRelation.Equal, view, NSLayoutAttribute.Height , 1f, 0f).WithAutomaticIdentifier().DisposeWith(_disposable),
-				};
-				_constraintsByView.Add(view, newConstraints);
-				return newConstraints;
-			}
+			_byCaseInfo = viewFactoryByCase.ToDictionary(
+				keySelector: kvp => kvp.Key,
+				elementSelector: kvp => new ViewInfo { NewViewFactory = kvp.Value }.DisposeWith(_disposable),
+				comparer: viewFactoryByCase.Comparer
+			);
 		}
 
 		public UIByCaseView<TCaseEnum> WithCase(TCaseEnum caseValue)
 		{
 			if (   !_currentCaseOnceSet
-				|| !_viewByCase.Comparer.Equals(_currentCase, caseValue))
+				|| !_byCaseInfo.Comparer.Equals(_currentCase, caseValue))
 			{
 				_currentCase = caseValue;
 				this.SetNeedsUpdateConstraints();//<-- request os to call method UpdateConstraints at the most appropriate moment.
@@ -57,15 +43,29 @@ namespace Xmf2.Core.iOS.Controls
 		{
 			base.UpdateConstraints();
 
-			var view = _viewByCase[_currentCase];
-			if (this.Subviews.FirstOrDefault() != view)
+			ViewInfo newInfo = _byCaseInfo[_currentCase];
+			if (newInfo != _currentInfo)
 			{
-				this.EnsureRemove(_constraintsByView.SelectMany(x => x.Value));
-				this.EnsureRemove(this.Subviews);
-				view.TranslatesAutoresizingMaskIntoConstraints = false;
-				this.AddSubview(view);
-				this.AddConstraints(GetContraintsFor(view));
+				this.EnsureRemove(_currentInfo?.GetContraintsFor(parentView: this));
+				this.EnsureRemove(_currentInfo?.GetView());
+				if (_aggressiveViewDispose)
+				{
+					_currentInfo?.DisposeConstraints();
+					_currentInfo?.DisposeView();
+				}
+
+				var newView = newInfo.GetView();
+				newView.TranslatesAutoresizingMaskIntoConstraints = false;
+				this.AddSubview(newView);
+				this.AddConstraints(newInfo.GetContraintsFor(parentView: this));
+				_currentInfo = newInfo;
 			}
+		}
+
+		public UIByCaseView<TCaseEnum> WithAggressiveViewDispose(bool aggressive = true)
+		{
+			_aggressiveViewDispose = aggressive;
+			return this;
 		}
 
 		protected override void Dispose(bool disposing)
@@ -73,11 +73,71 @@ namespace Xmf2.Core.iOS.Controls
 			if (disposing)
 			{
 				_currentCase = default(TCaseEnum);
-				_viewByCase = null;
-				_constraintsByView = null;
 				_disposable.Dispose();
 			}
 			base.Dispose(disposing);
+		}
+
+		private class ViewInfo : IDisposable
+		{
+			public Func<UIView> NewViewFactory { private get; set; }
+			private UIView _view;
+			private NSLayoutConstraint[] _viewConstraints;
+
+			internal UIView GetView() => this._view ?? (_view = NewViewFactory());
+
+			internal NSLayoutConstraint[] GetContraintsFor(UIView parentView)
+			{
+				if (_viewConstraints is null)
+				{
+					_viewConstraints = new NSLayoutConstraint[]
+					{
+						NSLayoutConstraint.Create(parentView, NSLayoutAttribute.CenterX , NSLayoutRelation.Equal, this.GetView(), NSLayoutAttribute.CenterX, 1f, 0f).WithAutomaticIdentifier(),
+						NSLayoutConstraint.Create(parentView, NSLayoutAttribute.CenterY , NSLayoutRelation.Equal, this.GetView(), NSLayoutAttribute.CenterY, 1f, 0f).WithAutomaticIdentifier(),
+						NSLayoutConstraint.Create(parentView, NSLayoutAttribute.Width   , NSLayoutRelation.Equal, this.GetView(), NSLayoutAttribute.Width  , 1f, 0f).WithAutomaticIdentifier(),
+						NSLayoutConstraint.Create(parentView, NSLayoutAttribute.Height  , NSLayoutRelation.Equal, this.GetView(), NSLayoutAttribute.Height , 1f, 0f).WithAutomaticIdentifier(),
+					};
+				}
+				return _viewConstraints;
+			}
+
+			internal void DisposeView()
+			{
+				_view?.Dispose();
+				_view = null;
+			}
+			internal void DisposeConstraints()
+			{
+				if (_viewConstraints is null)
+				{
+					return;
+				}
+				for (int i = 0; i < _viewConstraints.Length; i++)
+				{
+					_viewConstraints[i].Dispose();
+				}
+				_viewConstraints = null;
+			}
+
+			#region IDisposable Support
+
+			private bool _disposedValue = false;
+			protected virtual void Dispose(bool disposing)
+			{
+				if (!_disposedValue)
+				{
+					if (disposing)
+					{
+						NewViewFactory = null;
+						DisposeView();
+						DisposeConstraints();
+					}
+					_disposedValue = true;
+				}
+			}
+			public void Dispose() => Dispose(true);
+
+			#endregion IDisposable Support
 		}
 	}
 }
