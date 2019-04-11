3723d3fce913d64f3c9ea79f92789fd4a627079e
ma.coutanceau <ma.coutanceau@ideine.fr>
2019-04-11 17:54:48 +0200

    Updated EnumExtensions

--- src/Xmf2.Core/Extensions/EnumExtensions.cs
+++ src/Xmf2.Core/Extensions/EnumExtensions.cs
@@ -6,10 +6,21 @@
 		{
 			return new NotSupportedException(GetNotSupportedMessage(enumValue));
 		}
+		public static InvalidOperationException GetNotInvalidOperationException(this Enum enumValue)
+		{
+			return new InvalidOperationException(GetInvalidOperationMessage(enumValue));
+		}
 
 		public static string GetNotSupportedMessage(this Enum enumValue)
 		{
 			return string.Format("This {0} value is not supported yet. Value: {1}"
+				, enumValue.GetType().FullName
+				, enumValue);
+		}
+
+		public static string GetInvalidOperationMessage(this Enum enumValue)
+		{
+			return string.Format("This {0} value is not valid. Value: {1}"
 				, enumValue.GetType().FullName
 				, enumValue);
 		}
