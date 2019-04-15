ea662b5e88bdcfc649bef0112d6c71603f16812e
ma.coutanceau <ma.coutanceau@ideine.fr>
2019-04-15 13:40:39 +0200

    Pasage du emptyviewstate en singleton

---
+++ src/Xmf2.Components/ViewModels/EmptyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Xmf2.Components.Interfaces;
+
+namespace Xmf2.Components.ViewModels
+{
+	public class EmptyViewModel : BaseComponentViewModel
+	{
+		public EmptyViewModel(IServiceLocator services) : base(services)
+		{
+		}
+
+		protected override IViewState NewState() => EmptyViewState.Singleton;
+	}
+}
---
+++ src/Xmf2.Components/ViewModels/EmptyViewState.cs
@@ -0,0 +1,10 @@
+﻿using Xmf2.Components.Interfaces;
+
+namespace Xmf2.Components.ViewModels
+{
+	public class EmptyViewState : IViewState
+	{
+		public static readonly EmptyViewState Singleton = new EmptyViewState();
+		private EmptyViewState() { }
+	}
+}
