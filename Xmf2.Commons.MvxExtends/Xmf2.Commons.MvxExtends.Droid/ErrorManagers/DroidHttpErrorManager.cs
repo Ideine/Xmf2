156e695975ad76a68cfabdea3fb8c5dbcebf4969
Valentin Jubert <valentin.jubert@ideine.fr>
2019-09-30 10:39:02 +0200

    update polly

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Droid/ErrorManagers/DroidHttpErrorManager.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Droid/ErrorManagers/DroidHttpErrorManager.cs
@@ -1,72 +1,54 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Polly;
 using Xmf2.Commons.ErrorManagers;
 using Xmf2.Commons.MvxExtends.ErrorManagers;
 
 namespace Xmf2.Commons.MvxExtends.Droid.ErrorManagers
 {
-    public class DroidHttpErrorManager : BaseHttpErrorManager
-    {
-        Policy _droidHttpHandlePolicy;
+	public class DroidHttpErrorManager : BaseHttpErrorManager
+	{
+		private readonly IAsyncPolicy _droidHttpHandlePolicy;
 
-        public DroidHttpErrorManager()
-        {
-            _droidHttpHandlePolicy = Policy
-                    .Handle<System.Net.WebException>(webEx =>
-                                webEx.Status == System.Net.WebExceptionStatus.ConnectFailure
-                            || webEx.Status == System.Net.WebExceptionStatus.SendFailure
-                            || webEx.Status == System.Net.WebExceptionStatus.UnknownError
-                            || webEx.Status == System.Net.WebExceptionStatus.ConnectionClosed
-                            || webEx.Status == System.Net.WebExceptionStatus.KeepAliveFailure
-                            || webEx.Status == System.Net.WebExceptionStatus.PipelineFailure
-                            || webEx.Status == System.Net.WebExceptionStatus.ReceiveFailure
-                            || webEx.Status == System.Net.WebExceptionStatus.SecureChannelFailure
-                            || webEx.Status == System.Net.WebExceptionStatus.TrustFailure)
-                    .Or<Exception>(ex =>
-                                ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
-                            || ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
-                    .RetryAsync(3, (Action<Exception, int>)LogRetryException);
-        }
+		public DroidHttpErrorManager()
+		{
+			_droidHttpHandlePolicy = Policy
+					.Handle<WebException>(webEx =>
+								webEx.Status == WebExceptionStatus.ConnectFailure
+							|| webEx.Status == WebExceptionStatus.SendFailure
+							|| webEx.Status == WebExceptionStatus.UnknownError
+							|| webEx.Status == WebExceptionStatus.ConnectionClosed
+							|| webEx.Status == WebExceptionStatus.KeepAliveFailure
+							|| webEx.Status == WebExceptionStatus.PipelineFailure
+							|| webEx.Status == WebExceptionStatus.ReceiveFailure
+							|| webEx.Status == WebExceptionStatus.SecureChannelFailure
+							|| webEx.Status == WebExceptionStatus.TrustFailure)
+					.Or<Exception>(ex =>
+								ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
+							|| ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
+					.RetryAsync(3, LogRetryException);
+		}
 
-        protected override Policy GetHttpHandlePolicy()
-        {
-            return _droidHttpHandlePolicy;
-        }
+		protected override IAsyncPolicy GetHttpHandlePolicy()
+		{
+			return _droidHttpHandlePolicy;
+		}
 
-        protected override AccessDataException TreatException(Exception e)
-        {
-            var ade = e as AccessDataException;
-            if (ade != null)
-                return ade;
-
-            if (e is Java.Net.UnknownHostException)
-                return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, e);
-
-            var webEx = e as WebException;
-            if (webEx != null)
-            {
-                switch (webEx.Status)
-                {
-                    case WebExceptionStatus.Timeout:
-                        return new AccessDataException(AccessDataException.ErrorType.Timeout, webEx);
-                    case WebExceptionStatus.ConnectFailure:
-                        return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx);
-                    case WebExceptionStatus.NameResolutionFailure:
-                        return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx);
-                }
-            }
-            return base.TreatException(e);
-        }
-    }
+		protected override AccessDataException TreatException(Exception e)
+		{
+			switch (e)
+			{
+				case AccessDataException ade:
+					return ade;
+				case WebException webEx when webEx.Status == WebExceptionStatus.Timeout:
+					return new AccessDataException(AccessDataException.ErrorType.Timeout, webEx);
+				case WebException webEx when webEx.Status == WebExceptionStatus.ConnectFailure || webEx.Status == WebExceptionStatus.NameResolutionFailure:
+					return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx);
+				case Java.Net.UnknownHostException _:
+					return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, e);
+				default:
+					return base.TreatException(e);
+			}
+		}
+	}
 }
--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ErrorsManagers/TouchHttpErrorManager.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ErrorsManagers/TouchHttpErrorManager.cs
@@ -6,52 +6,47 @@ using Xmf2.Commons.ErrorManagers;
 
 namespace Xmf2.Commons.MvxExtends.Touch.ErrorManagers
 {
-	public class TouchHttpErrorManager: BaseHttpErrorManager
+	public class TouchHttpErrorManager : BaseHttpErrorManager
 	{
-		Policy _touchHttpHandlePolicy;
+		private readonly IAsyncPolicy _touchHttpHandlePolicy;
 
-		public TouchHttpErrorManager ()
+		public TouchHttpErrorManager()
 		{
 			_touchHttpHandlePolicy = Policy
-				.Handle<WebException> (webEx =>
-					webEx.Status == WebExceptionStatus.ConnectFailure
-					|| webEx.Status == WebExceptionStatus.SendFailure
-					|| webEx.Status == WebExceptionStatus.UnknownError
-					|| webEx.Status == WebExceptionStatus.ConnectionClosed
-					|| webEx.Status == WebExceptionStatus.KeepAliveFailure
-					|| webEx.Status == WebExceptionStatus.PipelineFailure
-					|| webEx.Status == WebExceptionStatus.ReceiveFailure
-					|| webEx.Status == WebExceptionStatus.SecureChannelFailure
-					|| webEx.Status == WebExceptionStatus.TrustFailure)
-				.Or<Exception> (ex =>
-					ex.Message.IndexOf ("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
-			        || ex.Message.IndexOf ("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
-				.RetryAsync (3, (Action<Exception, int>)LogRetryException);
+				.Handle<WebException>(webEx =>
+				   webEx.Status == WebExceptionStatus.ConnectFailure
+				   || webEx.Status == WebExceptionStatus.SendFailure
+				   || webEx.Status == WebExceptionStatus.UnknownError
+				   || webEx.Status == WebExceptionStatus.ConnectionClosed
+				   || webEx.Status == WebExceptionStatus.KeepAliveFailure
+				   || webEx.Status == WebExceptionStatus.PipelineFailure
+				   || webEx.Status == WebExceptionStatus.ReceiveFailure
+				   || webEx.Status == WebExceptionStatus.SecureChannelFailure
+				   || webEx.Status == WebExceptionStatus.TrustFailure)
+				.Or<Exception>(ex =>
+				   ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
+				   || ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
+				.RetryAsync(3, LogRetryException);
 		}
 
-		protected override Policy GetHttpHandlePolicy ()
+		protected override IAsyncPolicy GetHttpHandlePolicy()
 		{
 			return _touchHttpHandlePolicy;
 		}
 
-		protected override AccessDataException TreatException (Exception e)
+		protected override AccessDataException TreatException(Exception e)
 		{
-			var ade = e as AccessDataException;
-			if (ade != null)
-				return ade;
-
-            var webEx = e as WebException;
-			if (webEx != null) {
-				switch (webEx.Status) {
-				case WebExceptionStatus.Timeout:
-					return new AccessDataException (AccessDataException.ErrorType.Timeout, webEx);
-				case WebExceptionStatus.ConnectFailure:
-					return new AccessDataException (AccessDataException.ErrorType.NoInternetConnexion, webEx);
-				case WebExceptionStatus.NameResolutionFailure:
-					return new AccessDataException (AccessDataException.ErrorType.NoInternetConnexion, webEx);
-				}                 
+			switch (e)
+			{
+				case AccessDataException ade:
+					return ade;
+				case WebException webEx when webEx.Status == WebExceptionStatus.Timeout:
+					return new AccessDataException(AccessDataException.ErrorType.Timeout, webEx);
+				case WebException webEx when webEx.Status == WebExceptionStatus.ConnectFailure || webEx.Status == WebExceptionStatus.NameResolutionFailure:
+					return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx);
+				default:
+					return base.TreatException(e);
 			}
-			return base.TreatException (e);
 		}
 	}
 }
