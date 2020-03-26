40e2c215bc7f1bebce1f282db9649660c4bcbb32
Valentin Jubert <valentin.jubert@ideine.fr>
2020-03-26 09:54:51 +0100

    update polly

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ErrorsManagers/TouchHttpErrorManager.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/ErrorsManagers/TouchHttpErrorManager.cs
@@ -8,55 +8,38 @@ namespace Xmf2.Commons.MvxExtends.Touch.ErrorManagers
 {
 	public class TouchHttpErrorManager : BaseHttpErrorManager
 	{
-		Policy _touchHttpHandlePolicy;
+		private readonly IAsyncPolicy _touchHttpHandlePolicy;
 
 		public TouchHttpErrorManager()
 		{
 			_touchHttpHandlePolicy = Policy
 				.Handle<WebException>(webEx =>
-				   webEx.Status == WebExceptionStatus.ConnectFailure
-				   || webEx.Status == WebExceptionStatus.SendFailure
-				   || webEx.Status == WebExceptionStatus.UnknownError
-				   || webEx.Status == WebExceptionStatus.ConnectionClosed
-				   || webEx.Status == WebExceptionStatus.KeepAliveFailure
-				   || webEx.Status == WebExceptionStatus.PipelineFailure
-				   || webEx.Status == WebExceptionStatus.ReceiveFailure
-				   || webEx.Status == WebExceptionStatus.SecureChannelFailure
-				   || webEx.Status == WebExceptionStatus.TrustFailure)
+					webEx.Status == WebExceptionStatus.ConnectFailure
+					|| webEx.Status == WebExceptionStatus.SendFailure
+					|| webEx.Status == WebExceptionStatus.UnknownError
+					|| webEx.Status == WebExceptionStatus.ConnectionClosed
+					|| webEx.Status == WebExceptionStatus.KeepAliveFailure
+					|| webEx.Status == WebExceptionStatus.PipelineFailure
+					|| webEx.Status == WebExceptionStatus.ReceiveFailure
+					|| webEx.Status == WebExceptionStatus.SecureChannelFailure
+					|| webEx.Status == WebExceptionStatus.TrustFailure)
 				.Or<Exception>(ex =>
-				   ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
-				   || ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
-				.RetryAsync(3, (Action<Exception, int>)LogRetryException);
+					ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
+					|| ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
+				.RetryAsync(3, LogRetryException);
 		}
 
-		protected override Policy GetHttpHandlePolicy()
+		protected override IAsyncPolicy GetHttpHandlePolicy()
 		{
 			return _touchHttpHandlePolicy;
 		}
 
-		protected override AccessDataException TreatException(Exception e)
+		protected override AccessDataException TreatException(Exception e) => e switch
 		{
-			var ade = e as AccessDataException;
-			if (ade != null)
-			{
-				return ade;
-			}
-
-			var webEx = e as WebException;
-			if (webEx != null)
-			{
-				switch (webEx.Status)
-				{
-					case WebExceptionStatus.Timeout:
-						return new AccessDataException(AccessDataException.ErrorType.Timeout, webEx);
-					case WebExceptionStatus.ConnectFailure:
-						return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx);
-					case WebExceptionStatus.NameResolutionFailure:
-						return new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx);
-				}
-			}
-			return base.TreatException(e);
-		}
+			AccessDataException ade => ade,
+			WebException webEx when webEx.Status == WebExceptionStatus.Timeout => new AccessDataException(AccessDataException.ErrorType.Timeout, webEx),
+			WebException webEx when webEx.Status == WebExceptionStatus.ConnectFailure || webEx.Status == WebExceptionStatus.NameResolutionFailure => new AccessDataException(AccessDataException.ErrorType.NoInternetConnexion, webEx),
+			_ => base.TreatException(e)
+		};
 	}
 }
-
--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/ErrorManagers/BaseHttpErrorManager.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/ErrorManagers/BaseHttpErrorManager.cs
@@ -8,116 +8,123 @@ using Xmf2.Commons.MvxExtends.Logs;
 namespace Xmf2.Commons.MvxExtends.ErrorManagers
 {
 	public class BaseHttpErrorManager : IHttpErrorManager
-    {
-        Policy _httpHandlePolicy;
-        ILogger _logger;
+	{
+		private readonly IAsyncPolicy _httpHandlePolicy;
+		private readonly ILogger _logger;
 
-        public BaseHttpErrorManager()
-        {
-            _httpHandlePolicy = Policy
-                    .Handle<System.Net.WebException>(webEx =>
-                        webEx.Status == System.Net.WebExceptionStatus.ConnectFailure
-                        || webEx.Status == System.Net.WebExceptionStatus.SendFailure
-                        || webEx.Status == System.Net.WebExceptionStatus.UnknownError)
-                    .Or<Exception>(ex =>
-                        ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
-                        || ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
-                    .RetryAsync(3, (Action<Exception, int>)LogRetryException);
+		public BaseHttpErrorManager()
+		{
+			_httpHandlePolicy = Policy
+				.Handle<System.Net.WebException>(webEx =>
+					webEx.Status == System.Net.WebExceptionStatus.ConnectFailure
+					|| webEx.Status == System.Net.WebExceptionStatus.SendFailure
+					|| webEx.Status == System.Net.WebExceptionStatus.UnknownError)
+				.Or<Exception>(ex =>
+					ex.Message.IndexOf("Bad file descriptor", StringComparison.OrdinalIgnoreCase) != -1
+					|| ex.Message.IndexOf("Invalid argument", StringComparison.OrdinalIgnoreCase) != -1)
+				.RetryAsync(3, LogRetryException);
 
-            Mvx.TryResolve<ILogger>(out _logger);
-        }
+			Mvx.TryResolve(out _logger);
+		}
 
-        public virtual async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> action)
-        {
-            try
-            {
-                return
-                    await this.GetHttpHandlePolicy()
-                                .ExecuteAsync<TResult>(action)
-                                .ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                var ade = this.TreatException(e);
-                this.LogAccessDataException(ade);
-                throw ade;
-            }
-        }
+		public virtual async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> action)
+		{
+			try
+			{
+				return
+					await GetHttpHandlePolicy()
+						.ExecuteAsync(action)
+						.ConfigureAwait(false);
+			}
+			catch (Exception e)
+			{
+				var ade = TreatException(e);
+				LogAccessDataException(ade);
+				throw ade;
+			}
+		}
 
-        public virtual async Task ExecuteAsync(Func<Task> action)
-        {
-            try
-            {
-                await this.GetHttpHandlePolicy()
-                            .ExecuteAsync(action)
-                            .ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                var ade = this.TreatException(e);
-                this.LogAccessDataException(ade);
-                throw ade;
-            }
-        }
+		public virtual async Task ExecuteAsync(Func<Task> action)
+		{
+			try
+			{
+				await GetHttpHandlePolicy()
+					.ExecuteAsync(action)
+					.ConfigureAwait(false);
+			}
+			catch (Exception e)
+			{
+				var ade = TreatException(e);
+				LogAccessDataException(ade);
+				throw ade;
+			}
+		}
 
-        protected virtual Policy GetHttpHandlePolicy()
-        {
-            return _httpHandlePolicy;
-        }
+		protected virtual IAsyncPolicy GetHttpHandlePolicy()
+		{
+			return _httpHandlePolicy;
+		}
 
-        protected virtual AccessDataException TreatException(Exception e)
-        {
-            var ade = e as AccessDataException;
-            if (ade != null)
-                return ade;
+		protected virtual AccessDataException TreatException(Exception e)
+		{
+			if (e is AccessDataException ade)
+			{
+				return ade;
+			}
 
-            OperationCanceledException oce = e as OperationCanceledException;
-            if (oce != null)
-            {
-                if (oce.CancellationToken != null && !oce.CancellationToken.IsCancellationRequested)
-                    return new AccessDataException(AccessDataException.ErrorType.Timeout);
-            }
+			if (e is OperationCanceledException oce)
+			{
+				if (oce.CancellationToken != null && !oce.CancellationToken.IsCancellationRequested)
+				{
+					return new AccessDataException(AccessDataException.ErrorType.Timeout);
+				}
+			}
 
-            return new AccessDataException(AccessDataException.ErrorType.Unknown, e);
-        }
+			return new AccessDataException(AccessDataException.ErrorType.Unknown, e);
+		}
 
-        protected virtual void LogRetryException(Exception e, int retryCount)
-        {
-            _logger?.LogWarning(e, string.Format("HTTP retry attempt {0}.", retryCount));
-        }
+		protected virtual void LogRetryException(Exception e, int retryCount)
+		{
+			_logger?.LogWarning(e, $"HTTP retry attempt {retryCount}.");
+		}
 
-        protected virtual void LogAccessDataException(AccessDataException ade)
-        {
-            _logger?.LogError(ade);
-            ade.IsLogged = true;
-        }
+		protected virtual void LogAccessDataException(AccessDataException ade)
+		{
+			_logger?.LogError(ade);
+			ade.IsLogged = true;
+		}
 
-        protected bool TrueInAnyInnerException(Exception e, Func<Exception, bool> testForEachInnerException)
-        {
-            var currentException = e;
-            while (currentException != null)
-            {
-                if (testForEachInnerException(currentException))
-                    return true;
-                currentException = currentException.InnerException;
-            }
+		protected bool TrueInAnyInnerException(Exception e, Func<Exception, bool> testForEachInnerException)
+		{
+			var currentException = e;
+			while (currentException != null)
+			{
+				if (testForEachInnerException(currentException))
+				{
+					return true;
+				}
 
-            return false;
-        }
+				currentException = currentException.InnerException;
+			}
 
-        protected bool TrueInAnyInnerException<TException>(Exception e, Func<TException, bool> testForEachInnerException)
-            where TException : Exception
-        {
-            var currentException = e;
-            while (currentException != null)
-            {
-                var typedException = currentException as TException;
-                if (typedException != null && testForEachInnerException(typedException))
-                    return true;
-                currentException = currentException.InnerException;
-            }
+			return false;
+		}
 
-            return false;
-        }
-    }
+		protected bool TrueInAnyInnerException<TException>(Exception e, Func<TException, bool> testForEachInnerException)
+			where TException : Exception
+		{
+			var currentException = e;
+			while (currentException != null)
+			{
+				if (currentException is TException typedException && testForEachInnerException(typedException))
+				{
+					return true;
+				}
+
+				currentException = currentException.InnerException;
+			}
+
+			return false;
+		}
+	}
 }
