b9b813e45d247fd61f11a82053ad806c96b156a8
Valentin Jubert <valentin.jubert@ideine.fr>
2020-03-27 10:17:57 +0100

    update layoutextensions

--- Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/LayoutExtensions.cs
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Touch/AutoLayout/LayoutExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using UIKit;
-
 using static UIKit.NSLayoutAttribute;
 using static UIKit.NSLayoutRelation;
 
 public static class CustomAutoLayoutExtensions
 {
+	#region old
+
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static UIView CenterAndFillWidth(this UIView containerView, params UIView[] views)
 	{
@@ -29,12 +30,11 @@ public static class CustomAutoLayoutExtensions
 		return containerView;
 	}
 
-
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static UIView CenterAndFillWidth(this UIView containerView, UIView view, float margin = 0)
 	{
 		return containerView.WithConstraint(view, CenterX, Equal, containerView, CenterX, 1f, 0f)
-							.WithConstraint(view, Width, Equal, containerView, Width, 1f, -margin);
+			.WithConstraint(view, Width, Equal, containerView, Width, 1f, -margin);
 	}
 
 	public static void VerticalFlow(this UIView containerView, params UIView[] views)
@@ -52,7 +52,7 @@ public static class CustomAutoLayoutExtensions
 		containerView.AnchorTop(views[0]);
 		containerView.AnchorBottom(views[views.Length - 1]);
 
-		for (int i = 1; i < views.Length; ++i)
+		for (int i = 1 ; i < views.Length ; ++i)
 		{
 			containerView.VerticalSpace(views[i - 1], views[i]);
 		}
@@ -70,7 +70,7 @@ public static class CustomAutoLayoutExtensions
 			throw new ArgumentException("views must contains at least two elements", nameof(views));
 		}
 
-		for (int i = 1; i < views.Length; ++i)
+		for (int i = 1 ; i < views.Length ; ++i)
 		{
 			UIView v1 = views[i - 1];
 			UIView v2 = views[i];
@@ -148,7 +148,6 @@ public static class CustomAutoLayoutExtensions
 		return containerView;
 	}
 
-
 	public static UIView ConstrainHeight(this UIView view, int height)
 	{
 		view.ConstrainLayout(() => view.Height() == height);
@@ -224,9 +223,9 @@ public static class CustomAutoLayoutExtensions
 	public static UIView Same(this UIView view, UIView reference, UIView dest)
 	{
 		view.ConstrainLayout(() => reference.CenterY() == dest.CenterY()
-							 && reference.CenterX() == dest.CenterX()
-							 && reference.Height() == dest.Height()
-							 && reference.Width() == dest.Width());
+		                           && reference.CenterX() == dest.CenterX()
+		                           && reference.Height() == dest.Height()
+		                           && reference.Width() == dest.Width());
 		return view;
 	}
 
@@ -245,15 +244,84 @@ public static class CustomAutoLayoutExtensions
 	public static UIScrollView VerticalScrollContentConstraint(this UIScrollView scroll, UIView content)
 	{
 		scroll.ConstrainLayout(() => scroll.Left() == content.Left()
-							   && scroll.Right() == content.Right()
-							   && scroll.Top() == content.Top()
-							   && scroll.Bottom() == content.Bottom()
-							   && scroll.CenterX() == content.CenterX());
+		                             && scroll.Right() == content.Right()
+		                             && scroll.Top() == content.Top()
+		                             && scroll.Bottom() == content.Bottom()
+		                             && scroll.CenterX() == content.CenterX());
 		return scroll;
 	}
 
+	public static UIView NameConstraint(this UIView view, string name)
+	{
+#if DEBUG
+		view.ConstrainLayout(() => view.Name() == name);
+#endif
+
+		return view;
+	}
+
+	#endregion
+
+	#region new
+
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static UIView WithConstraint(this UIView constrainedView, UIView view1, NSLayoutAttribute attribute1, NSLayoutRelation relation, UIView view2, NSLayoutAttribute attribute2, nfloat multiplier, nfloat constant)
+	public static UIView WithRatio(this UIView view, float referenceWidth, float referenceHeight)
+	{
+		return view.WithRatio(view, referenceWidth, referenceHeight);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView WithRatio(this UIView constrainedView, UIView view, float referenceWidth, float referenceHeight)
+	{
+		return constrainedView.WithRatio(view, referenceWidth / referenceHeight);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView WithRatio(this UIView constrainedView, UIView view, float widthOnHeightRatio)
+	{
+		return constrainedView.WithConstraint(view, Width, Equal, view, Height, widthOnHeightRatio, 0f, nameof(WithRatio));
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView CenterAndLimitWidth(this UIView containerView, UIView view, float margin = 0)
+	{
+		return containerView.WithConstraint(view, CenterX, Equal, containerView, CenterX, 1f, 0f, $"{nameof(CenterAndLimitWidth)}-CenterX")
+			.WithConstraint(view, Width, LessThanOrEqual, containerView, Width, 1f, -margin, $"{nameof(CenterAndLimitWidth)}-Width");
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView MinVerticalSpace(this UIView containerView, UIView top, UIView bottom, float margin = 0)
+	{
+		return containerView.WithConstraint(bottom, Top, GreaterThanOrEqual, top, Bottom, 1f, margin, nameof(MinVerticalSpace));
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView ConstrainSize(this UIView view, float width, float height)
+	{
+		return view.WithConstraint(view, Width, Equal, 1f, width, $"{nameof(ConstrainSize)}-Width")
+			.WithConstraint(view, Height, Equal, 1f, height, $"{nameof(ConstrainSize)}-Height");
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView WithConstraint(this UIView constrainedView, UIView view, NSLayoutAttribute attribute, NSLayoutRelation relation, nfloat multiplier, nfloat constant, string identifier = null)
+	{
+		if (view != null && view != constrainedView)
+		{
+			view.TranslatesAutoresizingMaskIntoConstraints = false;
+		}
+
+		var constraint = NSLayoutConstraint.Create(view, attribute, relation, multiplier, constant);
+		if (identifier != null)
+		{
+			constraint.SetIdentifier(identifier);
+		}
+
+		constrainedView.AddConstraint(constraint);
+		return constrainedView;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static UIView WithConstraint(this UIView constrainedView, UIView view1, NSLayoutAttribute attribute1, NSLayoutRelation relation, UIView view2, NSLayoutAttribute attribute2, nfloat multiplier, nfloat constant, string identifier = null)
 	{
 		if (view1 != null && view1 != constrainedView)
 		{
@@ -265,22 +333,15 @@ public static class CustomAutoLayoutExtensions
 			view2.TranslatesAutoresizingMaskIntoConstraints = false;
 		}
 
-		constrainedView.AddConstraint(NSLayoutConstraint.Create(view1, attribute1, relation, view2, attribute2, multiplier, constant));
+		var constraint = NSLayoutConstraint.Create(view1, attribute1, relation, view2, attribute2, multiplier, constant);
+		if (identifier != null)
+		{
+			constraint.SetIdentifier(identifier);
+		}
+
+		constrainedView.AddConstraint(constraint);
 		return constrainedView;
 	}
 
-	#if DEBUG
-	public static UIView NameConstraint(this UIView view, string name)
-	{
-		view.ConstrainLayout(() => view.Name() == name);
-
-		return view;
-	}
-
-	#else
-	public static UIView NameConstraint(this UIView view, string name)
-	{
-		return view;
-	}
-	#endif
+	#endregion
 }
