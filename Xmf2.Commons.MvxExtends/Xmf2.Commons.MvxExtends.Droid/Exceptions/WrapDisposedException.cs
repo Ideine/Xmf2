e8a177e6b8ddc66aae02a0d0c8981c1076c3c461
mouhamadi <mouhamadi.mcolomari@ideine.fr>
2020-04-02 19:37:12 +0200

    Add WrapDisposedException

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends.Droid/Exceptions/WrapDisposedException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.OS;
+
+namespace Android
+{
+	public static class WrapDisposedException
+	{
+		public static void WrapForDisposedException(this object _, Action action, bool log = true)
+		{
+#if DEBUG
+			if (!IsInUiThread())
+			{
+				throw new Exception($"Developper misuse of {nameof(WrapForDisposedException)}");
+			}
+#endif
+			try
+			{
+				action();
+			}
+			catch (ObjectDisposedException e)
+			{
+				if (log)
+				{
+					System.Diagnostics.Debug.WriteLine(e);
+				}
+			}
+			catch (NullReferenceException nre)
+			{
+				if (log)
+				{
+					System.Diagnostics.Debug.WriteLine(nre);
+				}
+			}
+		}
+
+		public static bool IsInUiThread()
+		{
+			return Looper.MyLooper() == Looper.MainLooper;
+		}
+	}
+}
