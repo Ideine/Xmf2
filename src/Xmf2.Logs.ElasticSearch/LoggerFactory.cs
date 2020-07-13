403da97260b147a6f5579f77cd8a29ec8a2231c7
Marc Andre Coutanceau <ma.coutanceau@ideine.fr>
2020-07-13 11:49:59 +0200

    Adding log project

---
+++ src/Xmf2.Logs.ElasticSearch/LoggerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using Xmf2.Logs.ElasticSearch.Appenders;
+using Xmf2.Logs.ElasticSearch.Interfaces;
+using Xmf2.Logs.ElasticSearch.Senders;
+
+namespace Xmf2.Logs.ElasticSearch
+{
+	public static class LoggerFactory
+	{
+		public static IContextLogService Create(string index, string type, LogLevel minimalLogLevel, HttpClient client, string url, ILogBufferStorage storage)
+		{
+			return new ContextLogService(
+				new LogService(
+					new LogSender(client, url, storage),
+					minimalLogLevel
+				),
+				index,
+				type
+			).WithAppender(new TimestampLogAppender());
+		}
+	}
+}
---
+++ src/Xmf2.Logs.ElasticSearch/Senders/InMemoryLogBufferStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace Xmf2.Logs.ElasticSearch.Senders
+{
+	public class InMemoryLogBufferStorage : ILogBufferStorage
+	{
+		private string _content;
+
+		public string Load() => _content;
+
+		public void Save(string content) => _content = content;
+	}
+}
