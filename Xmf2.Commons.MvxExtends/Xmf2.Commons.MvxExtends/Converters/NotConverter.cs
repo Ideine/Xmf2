57b647ebc2b7611a748987704d55c59a8c3219fc
Valentin Jubert <valentin.jubert@ideine.fr>
2020-08-26 12:04:52 +0200

    revert delete of converter

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/NotConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace Xmf2.Commons.MvxExtends.Converters
+{
+	public class NotConverter : MvxValueConverter<bool, bool>
+	{
+		protected override bool Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return !value;
+		}
+	}
+}
---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/OrConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace Xmf2.Commons.MvxExtends.Converters
+{
+	public class OrConverter : MvxValueConverter<bool, bool>
+	{
+		protected override bool Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value || (bool)parameter;
+		}
+	}
+}
