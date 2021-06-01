db3c933182e52f37c2526e5f102930307c882fc8
Victor Deliege <victor.deliege@ideine.fr>
2021-06-01 16:10:33 +0200

    NotificationService

---
+++ src/Xmf2.Notification.Droid/IDroidNotificationConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Xmf2.Notification.Droid
+{
+	public interface IDroidNotificationConstants
+	{
+		string AppId { get; }
+		string ApiKey { get; }
+		string GcmId { get; }
+		string ProjectId { get; }
+	}
+}
