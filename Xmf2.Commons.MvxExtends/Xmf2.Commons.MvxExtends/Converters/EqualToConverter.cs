aa1c2e07ee351a12b6c32c6106e56e8fcb4e553f
Valentin Jubert <valentin.jubert@ideine.fr>
2021-08-16 17:19:49 +0200

    add equalto converter

---
+++ Xmf2.Commons.MvxExtends/Xmf2.Commons.MvxExtends/Converters/EqualToConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace Xmf2.Commons.MvxExtends.Converters
+{
+	public class EqualToConverter : MvxValueConverter<int, bool>
+	{
+		protected override bool Convert(int value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (parameter is int intParameter)
+			{
+				return value == intParameter;
+			}
+			else
+			{
+				return object.Equals(value, parameter);
+			}
+		}
+	}
+}
