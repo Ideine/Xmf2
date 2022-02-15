914e09d68c0053d8ff56e94ff851db5edfdff391
Valentin Jubert <valentin.jubert@ideine.fr>
2022-02-15 09:31:14 +0100

    add missing extensions

---
+++ Xmf2.Commons/Xmf2.Commons.iOS/Extensions/CreatorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UIKit;
+using Xmf2.Commons.iOS.Controls;
+using Xmf2.iOS.Extensions.Extensions;
+
+namespace Xmf2.Commons.iOS.Extensions
+{
+	public static class CreatorExtensions
+	{
+		public static UIHighlightButton CreateHighlightButton(this object parent)
+		{
+			return new UIHighlightButton();
+		}
+
+
+		public static TButton WithHighlightBackgroundColor<TButton>(this TButton button, int color)
+			where TButton : UIHighlightButton
+		{
+			button.HighlightColor = color.ColorFromHex();
+			return button;
+		}
+
+		public static TButton WithHighlightBackgroundColor<TButton>(this TButton button, UIColor color)
+			where TButton : UIHighlightButton
+		{
+			button.HighlightColor = color;
+			return button;
+		}
+	}
+}
