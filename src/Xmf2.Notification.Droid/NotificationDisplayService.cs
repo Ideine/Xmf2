85f302499c22a864faae17020537977467961206
Thierry Levisse <thierry.levisse@ideine.fr>
2023-04-27 09:48:17 +0200

    Fix notification always same extra intent

--- src/Xmf2.Notification.Droid/NotificationDisplayService.cs
+++ src/Xmf2.Notification.Droid/NotificationDisplayService.cs
@@ -8,88 +8,91 @@ using Firebase.Messaging;
 
 namespace Xmf2.Notification.Droid
 {
-	public interface INotificationDisplayService
-	{
-		void ShowNotification(FirebaseMessagingService context, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content);
-	}
+    public interface INotificationDisplayService
+    {
+        void ShowNotification(FirebaseMessagingService context, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content);
+    }
 
-	public abstract class BaseNotificationDisplayService : INotificationDisplayService
-	{
-		private readonly string _channelId;
+    public abstract class BaseNotificationDisplayService : INotificationDisplayService
+    {
+        private readonly Random _random;
 
-		// The user-visible name of the channel.
-		private readonly string _channelName;
+        private readonly string _channelId;
 
-		// The user-visible description of the channel.
-		private readonly string _channelDescription;
+        // The user-visible name of the channel.
+        private readonly string _channelName;
 
-		//The background color on notification channel
-		private readonly int _backgroundColor;
+        // The user-visible description of the channel.
+        private readonly string _channelDescription;
 
-		public BaseNotificationDisplayService(string channelId, string channelName, string channelDescription, int backgroundColor)
-		{
-			_channelId = channelId;
-			_channelName = channelName;
-			_channelDescription = channelDescription;
-			_backgroundColor = backgroundColor;
-		}
+        //The background color on notification channel
+        private readonly int _backgroundColor;
 
-		public virtual void ShowNotification(FirebaseMessagingService context, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content)
-		{
-			int pendingIntentId = (int)(DateTime.Now.Date.Millisecond & 0xFFFFFFF);
-			PendingIntent notificationContentIntent;
-			if (Build.VERSION.SdkInt >= BuildVersionCodes.S)
-			{
-				notificationContentIntent = PendingIntent.GetActivity(context, pendingIntentId, IntentForNotification(context, notification, notificationData, content), PendingIntentFlags.Mutable);
-			}
-			else
-			{
-				notificationContentIntent = PendingIntent.GetActivity(context, pendingIntentId, IntentForNotification(context, notification, notificationData, content), PendingIntentFlags.OneShot);
-			}
+        public BaseNotificationDisplayService(string channelId, string channelName, string channelDescription, int backgroundColor)
+        {
+            _random = new Random();
+            _channelId = channelId;
+            _channelName = channelName;
+            _channelDescription = channelDescription;
+            _backgroundColor = backgroundColor;
+        }
 
-			NotificationManager notificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
-			System.Diagnostics.Debug.Assert(notificationManager != null, nameof(notificationManager) + " != null");
+        public virtual void ShowNotification(FirebaseMessagingService context, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content)
+        {
+            int pendingIntentId = _random.Next();
+            PendingIntent notificationContentIntent;
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.S)
+            {
+                notificationContentIntent = PendingIntent.GetActivity(context, pendingIntentId, IntentForNotification(context, notification, notificationData, content), PendingIntentFlags.Mutable);
+            }
+            else
+            {
+                notificationContentIntent = PendingIntent.GetActivity(context, pendingIntentId, IntentForNotification(context, notification, notificationData, content), PendingIntentFlags.OneShot);
+            }
 
-			if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-			{
-				NotificationChannel notificationChannel = notificationManager.GetNotificationChannel(_channelId);
+            NotificationManager notificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
+            System.Diagnostics.Debug.Assert(notificationManager != null, nameof(notificationManager) + " != null");
 
-				if (notificationChannel == null)
-				{
-					notificationChannel = new NotificationChannel(_channelId, _channelName, NotificationImportance.Default)
-					{
-						Description = _channelDescription
-					};
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                NotificationChannel notificationChannel = notificationManager.GetNotificationChannel(_channelId);
 
-					notificationChannel.EnableLights(true);
-					// Sets the notification light color for notifications posted to this
-					// channel, if the device supports this feature.
-					notificationChannel.LightColor = 0x7F00FF00;
-					notificationManager.CreateNotificationChannel(notificationChannel);
-				}
-			}
+                if (notificationChannel == null)
+                {
+                    notificationChannel = new NotificationChannel(_channelId, _channelName, NotificationImportance.Default)
+                    {
+                        Description = _channelDescription
+                    };
 
-			NotificationCompat.Builder builder = new NotificationCompat.Builder(context, _channelId)
-				.SetStyle(new NotificationCompat.BigTextStyle().BigText(content))
-				.SetContentText(content)
-				.SetAutoCancel(true)
-				.SetContentIntent(notificationContentIntent)
-				.SetDefaults((int)NotificationDefaults.Vibrate)
-				.SetLights(0x7F00FF00, 100, 25);
+                    notificationChannel.EnableLights(true);
+                    // Sets the notification light color for notifications posted to this
+                    // channel, if the device supports this feature.
+                    notificationChannel.LightColor = 0x7F00FF00;
+                    notificationManager.CreateNotificationChannel(notificationChannel);
+                }
+            }
 
-			if (_backgroundColor != -1)
-			{
-				builder.SetColor(_backgroundColor);
-			}
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, _channelId)
+                .SetStyle(new NotificationCompat.BigTextStyle().BigText(content))
+                .SetContentText(content)
+                .SetAutoCancel(true)
+                .SetContentIntent(notificationContentIntent)
+                .SetDefaults((int)NotificationDefaults.Vibrate)
+                .SetLights(0x7F00FF00, 100, 25);
 
-			BuildNotification(context, builder, notification, notificationData, content);
+            if (_backgroundColor != -1)
+            {
+                builder.SetColor(_backgroundColor);
+            }
 
-			// Finally publish the notification
-			notificationManager.Notify(DateTime.Now.Millisecond, builder.Build());
-		}
+            BuildNotification(context, builder, notification, notificationData, content);
 
-		protected abstract Intent IntentForNotification(FirebaseMessagingService context, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content);
+            // Finally publish the notification
+            notificationManager.Notify(DateTime.Now.Millisecond, builder.Build());
+        }
 
-		protected abstract void BuildNotification(FirebaseMessagingService context, NotificationCompat.Builder builder, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content);
-	}
+        protected abstract Intent IntentForNotification(FirebaseMessagingService context, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content);
+
+        protected abstract void BuildNotification(FirebaseMessagingService context, NotificationCompat.Builder builder, RemoteMessage.Notification notification, IDictionary<string, string> notificationData, string content);
+    }
 }
