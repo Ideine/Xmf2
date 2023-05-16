245a86b20a8429e96f6af1a42526afccd96b6364
Valentin Jubert <valentin.jubert@ideine.fr>
2023-05-16 16:45:18 +0200

    add missing java.lang.object ctor

--- src/Xmf2.Core.Droid/Helpers/UnderlineTouchListener.cs
+++ src/Xmf2.Core.Droid/Helpers/UnderlineTouchListener.cs
@@ -1,4 +1,6 @@
-﻿using Android.Graphics;
+﻿using System;
+using Android.Graphics;
+using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
@@ -6,6 +8,9 @@ namespace Xmf2.Core.Droid.Helpers
 {
 	public class UnderlineTouchListener : Java.Lang.Object, View.IOnTouchListener
 	{
+		public UnderlineTouchListener() { }
+		protected UnderlineTouchListener(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer) { }
+
 		public bool OnTouch(View v, MotionEvent e)
 		{
 			var cgu = (TextView)v;
