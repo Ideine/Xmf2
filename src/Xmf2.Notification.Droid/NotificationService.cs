b1fe655c17103233a4bf1e4e0f7021048ff99620
Christine Lasquellec <christine.lasquellec@ideine.fr>
2023-09-06 11:30:07 +0200

    add net7 support for NotificationService android

--- src/Xmf2.Notification.Droid/NotificationService.cs
+++ src/Xmf2.Notification.Droid/NotificationService.cs
@@ -11,7 +11,11 @@ using Android.Util;
 using Firebase;
 using Firebase.Installations;
 using Firebase.Messaging;
+#if NET7_0_OR_GREATER
+using Microsoft.Maui.ApplicationModel;
+#else
 using Plugin.CurrentActivity;
+#endif
 using Xmf2.Core.Droid.Permissions;
 using Xmf2.Core.Services;
 
@@ -41,7 +45,11 @@ namespace Xmf2.Notification.Droid
 		{
 			if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu)
 			{
-				Activity activity = CrossCurrentActivity.Current.Activity;
+#if NET7_0_OR_GREATER
+				Activity activity = Platform.CurrentActivity;
+#else
+				var activity = CrossCurrentActivity.Current.Activity;
+#endif
 				if (activity.CheckSelfPermission(Manifest.Permission.PostNotifications) != Permission.Granted)
 				{
 					bool result = true;
