d06ec0ee35fe292cfef1690f7e22e53dcb83aa86
Valentin Jubert <valentin.jubert@ideine.fr>
2023-11-10 10:47:20 +0100

    add image view extensions

---
+++ src/Xmf2.Core.Droid/Extensions/ImageViewExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Android.Widget;
+
+namespace Xmf2.Core.Droid.Extensions
+{
+	public static class ImageViewExtensions
+	{
+		public static string WithAspect(this string url, ImageView.ScaleType scaleType)
+		{
+			string parameterChar = url.Contains('?') ? "&" : "?";
+
+			if (scaleType == ImageView.ScaleType.FitXy || scaleType == ImageView.ScaleType.CenterCrop)
+			{
+				return url + parameterChar + "aspect=Fill";
+			}
+			else if (scaleType == ImageView.ScaleType.FitCenter
+					 || scaleType == ImageView.ScaleType.Center
+					 || scaleType == ImageView.ScaleType.CenterInside
+					 || scaleType == ImageView.ScaleType.FitEnd
+					 || scaleType == ImageView.ScaleType.FitStart
+					 || scaleType == ImageView.ScaleType.FitEnd)
+			{
+				return url + parameterChar + "aspect=Fit";
+			}
+
+			return url + parameterChar + "aspect=Undefined";
+		}
+	}
+}
+
