ef10b09dd29f77e3cb50e113b35bd8b0f57d939f
Valentin Jubert <valentin.jubert@ideine.fr>
2023-12-19 16:43:55 +0100

    correction imageview extensions

--- src/Xmf2.Core.iOS/Extensions/ImageViewExtensions.cs
+++ src/Xmf2.Core.iOS/Extensions/ImageViewExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UIKit;
 
 namespace Xmf2.Core.iOS.Extensions
@@ -7,8 +8,9 @@ namespace Xmf2.Core.iOS.Extensions
 	{
 		public static (int width, int height) GetImageSize(this UIView view)
 		{
-			int height = (int?)view.Constraints.FirstOrDefault(x => x.FirstAttribute == NSLayoutAttribute.Height && x.Relation is NSLayoutRelation.Equal or NSLayoutRelation.LessThanOrEqual)?.Constant ?? 0;
-			int width = (int?)view.Constraints.FirstOrDefault(x => x.FirstAttribute == NSLayoutAttribute.Width && x.Relation is NSLayoutRelation.Equal or NSLayoutRelation.LessThanOrEqual)?.Constant ?? 0;
+			IEnumerable<NSLayoutConstraint> activeConstraints = view.Constraints.Where(x => x.Active && !x.Description.Contains("NSContentSizeLayoutConstraint"));
+			int height = (int?)activeConstraints.FirstOrDefault(x => x.FirstAttribute == NSLayoutAttribute.Height && x.Relation is NSLayoutRelation.Equal or NSLayoutRelation.LessThanOrEqual)?.Constant ?? 0;
+			int width = (int?)activeConstraints.FirstOrDefault(x => x.FirstAttribute == NSLayoutAttribute.Width && x.Relation is NSLayoutRelation.Equal or NSLayoutRelation.LessThanOrEqual)?.Constant ?? 0;
 
 			return (width, height);
 		}
