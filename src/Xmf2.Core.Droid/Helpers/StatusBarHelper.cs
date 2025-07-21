3169719d69746534b3b5e441352c1a6376b02362
Benjamin-Leroux <benjamin.leroux59@gmail.com>
2025-07-21 17:02:33 +0200

    revert to old statusbar helper

--- src/Xmf2.Core.Droid/Helpers/StatusBarHelper.cs
+++ src/Xmf2.Core.Droid/Helpers/StatusBarHelper.cs
@@ -1,41 +1,58 @@
-﻿using Android.OS;
+﻿using System;
+using Android.Content;
+using Android.Runtime;
 using Android.Views;
+using AndroidX.Core.Graphics;
 using AndroidX.Core.View;
 
 namespace Xmf2.Core.Droid.Helpers
 {
 	public class StatusBarHelper : Java.Lang.Object, IOnApplyWindowInsetsListener
 	{
-		public StatusBarHelper(View container)
+		private static int _topPadding;
+		private Context _context;
+		private View _container;
+		private readonly int _paddingTopInDp;
+
+		public StatusBarHelper(Context context, View container, int paddingTopInDp = 12)
 		{
+			_context = context;
+			_container = container;
+			_paddingTopInDp = paddingTopInDp;
+			SetPaddingTopOnContainer();
 			ViewCompat.SetOnApplyWindowInsetsListener(container, this);
+		}
+
+		protected StatusBarHelper(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer) { }
+
+		private void SetPaddingTopOnContainer()
+		{
+			_container.SetPadding(_container.PaddingStart, _topPadding, _container.PaddingEnd, _container.PaddingBottom);
 		}
 
 		public WindowInsetsCompat OnApplyWindowInsets(View v, WindowInsetsCompat insets)
 		{
-			int left, top, right, bottom;
-
-			if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+			if (_context != null && _container != null)
 			{
-				// Marche a partir de Android 11 (API 30)
-				var sysInsets = insets.GetInsets(WindowInsets.Type.SystemBars());
-				left = sysInsets.Left;
-				top = sysInsets.Top;
-				right = sysInsets.Right;
-				bottom = sysInsets.Bottom;
+				Insets statusBarInset = insets.GetInsets(WindowInsetsCompat.Type.StatusBars());
+				Insets displayCutoutInset = insets.GetInsets(WindowInsetsCompat.Type.DisplayCutout());
+				int paddingTop = UIHelper.DpToPx(_context, _paddingTopInDp) + Math.Max(statusBarInset.Top, displayCutoutInset.Top);
+				_topPadding = paddingTop;
+				SetPaddingTopOnContainer();
 			}
-			else
-			{
-				// Marche jusqu'a Android 10 (API 29)
-				left = insets.SystemWindowInsetLeft;
-				top = insets.SystemWindowInsetTop;
-				right = insets.SystemWindowInsetRight;
-				bottom = insets.SystemWindowInsetBottom;
-			}
-
-			v.SetPadding(left, top, right, bottom);
 
 			return insets;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_context = null;
+				_container = null;
+			}
+
+			base.Dispose(disposing);
 		}
 	}
 }
